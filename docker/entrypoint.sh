cc3828a9fb6a53edbf6e9d9b29c2e06dd7bd067b
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-09-20 16:47:02 +0000

    pass the variables

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,8 @@
 
 source /home/cdkuser/.asdf/asdf.sh
 
+epsZoneId=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "eps-route53-resources:EPS-ZoneID") | .Value' | grep -o '[^:]*$')
+epsDomain=$(aws cloudformation list-exports --output json | jq -r '.Exports[] | select(.Name == "eps-route53-resources:EPS-domain") | .Value' | grep -o '[^:]*$')
+export epsZoneId
+export epsDomain
 make cdk-deploy
