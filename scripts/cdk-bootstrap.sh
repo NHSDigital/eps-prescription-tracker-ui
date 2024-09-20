0ddb02841df5f3e621b9bce47b1dd9dd92e5ac16
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-09-20 16:34:51 +0000

    change policy name

--- scripts/cdk-bootstrap.sh
+++ scripts/cdk-bootstrap.sh
@@ -6,7 +6,7 @@ GrantCloudFormationExecutionAccessPolicyA=arn:aws:iam::591291862413:policy/ci-re
 GrantCloudFormationExecutionAccessPolicyB=arn:aws:iam::591291862413:policy/ci-resources-GrantCloudFormationExecutionAccessPolicyB-Yhazzqk3hi9H
 GrantCloudFormationExecutionAccessPolicyC=arn:aws:iam::591291862413:policy/ci-resources-GrantCloudFormationExecutionAccessPolicyC-B5O11HpNZ1dl
 # KMSKeyPolicy=arn:aws:iam::591291862413:policy/account-resources-UseArtifactBucketKMSKeyManagedPolicy-T1xeJJC1dsMk
-GrantCognitorPolicy=arn:aws:iam::591291862413:policy/ab_cognito_test
+GrantCloudFormationExecutionAccessPolicyD=arn:aws:iam::591291862413:policy/ab_cognito_test
 # ArtifactBucketKeyArn=arn:aws:kms:eu-west-2:591291862413:key/91f11a0a-255d-4f25-9d5d-e2711d3c4b20
 
 # this should be retrieved automatically
@@ -16,7 +16,7 @@ ALL_POLICIES=${GrantCloudFormationExecutionAccessIAMPolicy}
 ALL_POLICIES="${ALL_POLICIES},${GrantCloudFormationExecutionAccessPolicyA}"
 ALL_POLICIES="${ALL_POLICIES},${GrantCloudFormationExecutionAccessPolicyB}"
 ALL_POLICIES="${ALL_POLICIES},${GrantCloudFormationExecutionAccessPolicyC}"
-ALL_POLICIES="${ALL_POLICIES},${GrantCognitorPolicy}"
+ALL_POLICIES="${ALL_POLICIES},${GrantCloudFormationExecutionAccessPolicyD}"
 #ALL_POLICIES="${ALL_POLICIES},${KMSKeyPolicy}"
 
 
