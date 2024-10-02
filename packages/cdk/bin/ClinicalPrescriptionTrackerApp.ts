bfcf07bfec9baa2f6b1dfad835ca4745eb6a014a
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-10-02 11:36:01 +0000

    add code to add metadata if needed

--- packages/cdk/bin/ClinicalPrescriptionTrackerApp.ts
+++ packages/cdk/bin/ClinicalPrescriptionTrackerApp.ts
@@ -20,14 +20,60 @@ Tags.of(app).add("version", version)
 Tags.of(app).add("stackName", stackName)
 Tags.of(app).add("commit", commit)
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const USCertificates = new USCertificatesStack(app, "USCertificates", {
   env: {region: "us-east-1"},
   stackName: stackName
 })
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ClinicalPrescriptionTracker = new ClinicalPrescriptionTrackerStack(app, "ClinicalPrescriptionTrackerStack", {
   env: {region: "eu-west-2"},
   stackName: stackName
 })
+
+// run a synth to add cross region lambdas and roles
+app.synth()
+
+// add metadata to lambda so they dont get flagged as failing cfn-guard
+addCfnGuardMetadata(USCertificates, "Custom::CrossRegionExportWriterCustomResourceProvider")
+addCfnGuardMetadata(ClinicalPrescriptionTracker, "Custom::CrossRegionExportReaderCustomResourceProvider")
+
+// finally run synth again with force to include the added metadata
+app.synth({
+  force: true
+})
+
+// function which adds metadata to ignore things which fail cfn-guard
+function addCfnGuardMetadata(stack: cdk.Stack, role: string) {
+  const writerProvider = stack.node.tryFindChild(role)
+  if (writerProvider === undefined) {
+    return
+  }
+  const writerLambda = writerProvider.node.tryFindChild("Handler") as cdk.CfnResource
+  const writerRole = writerProvider.node.tryFindChild("Role") as cdk.CfnResource
+  if (writerLambda !== undefined) {
+    writerLambda.cfnOptions.metadata = (
+      {
+        ...writerLambda.cfnOptions.metadata,
+        "guard": {
+          "SuppressedRules": [
+            "LAMBDA_DLQ_CHECK",
+            "LAMBDA_INSIDE_VPC",
+            "LAMBDA_CONCURRENCY_CHECK"
+          ]
+        }
+      }
+    )
+  }
+  if (writerRole !== undefined) {
+    writerRole.cfnOptions.metadata = (
+      {
+        ...writerLambda.cfnOptions.metadata,
+        "guard": {
+          "SuppressedRules": [
+            "IAM_NO_INLINE_POLICY_CHECK"
+          ]
+        }
+      }
+    )
+  }
+}
