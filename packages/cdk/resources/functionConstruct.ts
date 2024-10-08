b0da3b5adef5cedf8273fcf240efbf490fb5bcc4
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-10-08 16:32:48 +0000

    add function construct

---
+++ packages/cdk/resources/functionConstruct.ts
@@ -0,0 +1,148 @@
+import * as cdk from "aws-cdk-lib"
+import * as iam from "aws-cdk-lib/aws-iam"
+import * as logs from "aws-cdk-lib/aws-logs"
+import * as nodeLambda from "aws-cdk-lib/aws-lambda-nodejs"
+import * as kms from "aws-cdk-lib/aws-kms"
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import {Construct} from "constructs"
+
+import {getDefaultLambdaOptions} from "./helpers"
+
+const insightsLayerArn = "arn:aws:lambda:eu-west-2:580247275435:layer:LambdaInsightsExtension:53"
+
+export interface FunctionConstructProps {
+  readonly stackName: string;
+  readonly lambdaName: string;
+  readonly additionalPolicies?: Array<iam.IManagedPolicy>;
+  readonly logRetentionInDays: number;
+  readonly packageBasePath: string;
+  readonly entryPoint: string;
+  readonly lambdaEnvironmentVariables: { [key: string]: string; }
+}
+
+/**
+ * Resources for a lambda
+
+ */
+export class FunctionConstruct extends Construct {
+  public readonly executeLambdaManagedPolicy: iam.ManagedPolicy
+  public readonly lambda: nodeLambda.NodejsFunction
+
+  public constructor(scope: Construct, id: string, props: FunctionConstructProps) {
+    super(scope, id)
+
+    // Resources
+
+    const cloudWatchLogsKmsKey = kms.Key.fromKeyArn(
+      this,
+      "cloudWatchLogsKmsKey",
+      cdk.Fn.importValue("account-resources:CloudwatchLogsKmsKeyArn")
+    )
+    const lambdaLogGroup = new logs.LogGroup(this, "LambdaLogGroup", {
+      encryptionKey: cloudWatchLogsKmsKey,
+      logGroupName: `/aws/lambda/${props.lambdaName!}`,
+      retention: props.logRetentionInDays,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    })
+
+    const cfnlambdaLogGroup = lambdaLogGroup.node.defaultChild as logs.CfnLogGroup
+    cfnlambdaLogGroup.cfnOptions.metadata = {
+      guard: {
+        SuppressedRules: [
+          "CW_LOGGROUP_RETENTION_PERIOD_CHECK"
+        ]
+      }
+    }
+
+    const lambdaManagedPolicy = new iam.ManagedPolicy(this, `Execute${props.lambdaName}ManagedPolicy`, {
+      description: `write to ${props.lambdaName} logs`,
+      statements: [
+        new iam.PolicyStatement({
+          actions: [
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          resources: [
+            lambdaLogGroup.logGroupArn,
+            `${lambdaLogGroup.logGroupArn}:log-stream:*`
+          ]
+        })]
+    })
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const lambdaSplunkSubscriptionFilter = new logs.CfnSubscriptionFilter(this, "LambdaSplunkSubscriptionFilter", {
+      roleArn: cdk.Fn.importValue("lambda-resources:SplunkSubscriptionFilterRole"),
+      logGroupName: lambdaLogGroup.logGroupName,
+      filterPattern: "",
+      destinationArn: cdk.Fn.importValue("lambda-resources:SplunkDeliveryStream")
+    })
+
+    const lambdaInsightsLogGroupPolicy = iam.ManagedPolicy.fromManagedPolicyArn(
+      this,
+      "lambdaInsightsLogGroupPolicy",
+      cdk.Fn.importValue("lambda-resources:LambdaInsightsLogGroupPolicy")
+    )
+
+    const cloudwatchEncryptionKMSPolicyArn = iam.ManagedPolicy.fromManagedPolicyArn(
+      this,
+      "cloudwatchEncryptionKMSPolicyArn",
+      cdk.Fn.importValue("account-resources:CloudwatchEncryptionKMSPolicyArn")
+    )
+    const lambdaDecryptSecretsKMSPolicy = iam.ManagedPolicy.fromManagedPolicyArn(
+      this,
+      "lambdaDecryptSecretsKMSPolicy",
+      cdk.Fn.importValue("account-resources:LambdaDecryptSecretsKMSPolicy")
+    )
+    const lambdaRole = new iam.Role(this, "LambdaRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      managedPolicies: [
+        lambdaManagedPolicy,
+        lambdaInsightsLogGroupPolicy,
+        cloudwatchEncryptionKMSPolicyArn,
+        lambdaDecryptSecretsKMSPolicy,
+        ...(props.additionalPolicies ?? [])
+      ]
+    })
+
+    const lambdaOptions = getDefaultLambdaOptions({
+      functionName: `${props.stackName}-${props.lambdaName}`,
+      packageBasePath: props.packageBasePath,
+      entryPoint: props.entryPoint
+    })
+
+    const insightsLambdaLayer = lambda.LayerVersion.fromLayerVersionArn(this, "LayerFromArn", insightsLayerArn)
+    const lambdaFunction = new nodeLambda.NodejsFunction(this, props.lambdaName, {
+      ...lambdaOptions,
+      role: lambdaRole,
+      environment: props.lambdaEnvironmentVariables,
+      logGroup: lambdaLogGroup,
+      layers: [
+        insightsLambdaLayer
+      ]
+    })
+
+    const cfnLambda = lambdaFunction.node.defaultChild as lambda.CfnFunction
+    cfnLambda.cfnOptions.metadata = {
+      "guard": {
+        "SuppressedRules": [
+          "LAMBDA_DLQ_CHECK",
+          "LAMBDA_INSIDE_VPC",
+          "LAMBDA_CONCURRENCY_CHECK"
+        ]
+      }
+    }
+
+    const executeLambdaManagedPolicy = new iam.ManagedPolicy(this, "ExecuteLambdaManagedPolicy", {
+      description: `execute lambda ${props.lambdaName}`,
+      statements: [
+        new iam.PolicyStatement({
+          actions: ["lambda:InvokeFunction"],
+          resources: [lambdaFunction.functionArn]
+        })]
+    })
+
+    // Outputs
+    this.lambda = lambdaFunction
+    this.executeLambdaManagedPolicy = executeLambdaManagedPolicy
+  }
+}
---
+++ packages/cdk/resources/helpers.ts
@@ -0,0 +1,46 @@
+import {Duration} from "aws-cdk-lib"
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import * as nodeLambda from "aws-cdk-lib/aws-lambda-nodejs"
+
+import * as path from "path"
+
+const baseDir = path.resolve(__dirname, "../../..")
+
+function getLambdaInvokeURL(region: string, lambdaNArn: string) {
+  return `arn:aws:apigateway:${region}:lambda:path/2015-03-31/functions/${lambdaNArn}/invocations`
+}
+
+function getLambdaArn(region: string, account: string, lambdaName: string) {
+  return `arn:aws:lambda:${region}:${account}:function:${lambdaName}`
+}
+interface defaultLambdaOptionsParams {
+    readonly functionName: string;
+    readonly packageBasePath: string;
+    readonly entryPoint: string;
+  }
+
+function getDefaultLambdaOptions(options: defaultLambdaOptionsParams): nodeLambda.NodejsFunctionProps {
+  const defaultOptions: nodeLambda.NodejsFunctionProps = {
+    functionName: options.functionName,
+    runtime: lambda.Runtime.NODEJS_20_X,
+    entry: path.join(baseDir, options.packageBasePath, options.entryPoint),
+    projectRoot: baseDir,
+    memorySize: 256,
+    timeout: Duration.seconds(50),
+    architecture: lambda.Architecture.X86_64,
+    handler: "handler",
+    bundling: {
+      minify: true,
+      sourceMap: true,
+      tsconfig: path.join(baseDir, options.packageBasePath, "tsconfig.json"),
+      target: "es2020"
+    }
+  }
+  return defaultOptions
+}
+
+export {
+  getLambdaInvokeURL,
+  getDefaultLambdaOptions,
+  getLambdaArn
+}
