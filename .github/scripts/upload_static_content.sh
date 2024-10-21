7a164a67f84cec2b36f1fba45c7cee96446df8e7
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-10-21 08:02:43 +0000

    debug

--- .github/scripts/upload_static_content.sh
+++ .github/scripts/upload_static_content.sh
@@ -2,6 +2,15 @@
 
 staticBucketName=$(aws cloudformation list-exports --region eu-west-2 --output json | jq -r '.Exports[] | select(.Name == "cpt-ui-shared-resources:StaticContentBucket:bucketName") | .Value' )
 
+echo "staticBucketName: ${staticBucketName}"
+
 root_path=../../.build
+
+echo "ls root path"
+ls $root_path
+
+echo "ls -R root path packages"
+ls -R ${root_path}/packages/staticContent
+
 aws s3 cp "${root_path}/packages/staticContent/404.html" "${staticBucketName}/404.html"
-aws s3 cp "${root_path}/build/packages/staticContent/jwks/dev/jwks.json" "${staticBucketName}/jwks.json" 
+aws s3 cp "${root_path}/packages/staticContent/jwks/dev/jwks.json" "${staticBucketName}/jwks.json" 
