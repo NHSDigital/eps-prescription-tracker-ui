9c0c75959805f608e7a51255fc1e5ce296f7dafc
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-10-21 11:29:16 +0000

    get the correct path

--- .github/scripts/upload_static_content.sh
+++ .github/scripts/upload_static_content.sh
@@ -10,10 +10,14 @@ if [ -z "${SHARED_RESOURCES_STACK_NAME}" ]; then
     echo "SHARED_RESOURCES_STACK_NAME is unset or set to the empty string"
     exit 1
 fi
-ls "${ROOT_PATH}"
 
 staticBucketName=$(aws cloudformation list-exports --query "Exports[?Name=='$SHARED_RESOURCES_STACK_NAME:StaticContentBucket:bucketName'].Value" --output text)
 
-echo "uploading content"
+if [ -z "${staticBucketName}" ]; then
+    echo "could not retrieve staticBucketName"
+    exit 1
+fi
+
+echo "Uploading static content from ${ROOT_PATH} to s3://${staticBucketName}"
 aws s3 cp "${ROOT_PATH}/packages/staticContent/404.html" "s3://${staticBucketName}/404.html"
 aws s3 cp "${ROOT_PATH}/packages/staticContent/jwks/dev/jwks.json" "s3://${staticBucketName}/jwks.json"
