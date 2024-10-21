5f24cf75a8ad318366d308e11cfd862c98e3ea86
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-10-21 08:45:58 +0000

    fix script

--- .github/scripts/upload_static_content.sh
+++ .github/scripts/upload_static_content.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
+if [ -z "${ROOT_PATH}" ]; then
+    echo "ROOT_PATH is unset or set to the empty string"
+    exit 1
+fi
 staticBucketName=$(aws cloudformation list-exports --region eu-west-2 --output json | jq -r '.Exports[] | select(.Name == "cpt-ui-shared-resources:StaticContentBucket:bucketName") | .Value' )
 
-root_path=".build"
-
-aws s3 cp "${root_path}/packages/staticContent/404.html" "${staticBucketName}/404.html"
-aws s3 cp "${root_path}/packages/staticContent/jwks/dev/jwks.json" "${staticBucketName}/jwks.json" 
+aws s3 cp "${ROOT_PATH}/packages/staticContent/404.html" "s3://${staticBucketName}/404.html"
+aws s3 cp "${ROOT_PATH}/packages/staticContent/jwks/dev/jwks.json" "s3://${staticBucketName}/jwks.json"
