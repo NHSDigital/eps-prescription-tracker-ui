1cfd8f31ae2197f3ffc1b073ae20f74d6a1becb2
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-11-02 16:30:28 +0000

    more nag supression

--- packages/cdk/nagSuppressions.ts
+++ packages/cdk/nagSuppressions.ts
@@ -129,6 +129,16 @@ export const nagSuppressions = (stack: Stack) => {
         }
       ]
     )
+    safeAddNagSuppression(
+      stack,
+      "/StatelessStack/CognitoFunctions/MockTokenResources/LambdaPutLogsManagedPolicy/Resource",
+      [
+        {
+          id: "AwsSolutions-IAM5",
+          reason: "Suppress error for not having wildcards in permissions. This is a fine as we need to have permissions on all log streams under path"
+        }
+      ]
+    )
 
   }
 }
