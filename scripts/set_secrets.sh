13ad942d8ad8eb7f47c94098392040e4881eacb5
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2024-11-04 14:57:58 +0000

    fix lint

--- scripts/set_secrets.sh
+++ scripts/set_secrets.sh
@@ -8,11 +8,10 @@ check_gh_logged_in() {
 }
 
 set_secrets() {
-
     gh secret set primaryOidcClientId \
         --repo NHSDigital/eps-prescription-tracker-ui \
         --app actions \
-        --body "$Cis2PTLClientID"
+        --body "${Cis2PTLClientID}"
 
     gh secret set primaryOidClientSecret \
         --repo NHSDigital/eps-prescription-tracker-ui \
@@ -31,5 +30,23 @@ set_secrets() {
         --app actions \
         --body "$mockClientSecret"
 }
+
+if [ -z "${Cis2PTLClientID}" ]; then
+    echo "Cis2PTLClientID is unset or set to the empty string"
+    exit 1
+fi
+if [ -z "${Cis2PTLClientSecret}" ]; then
+    echo "Cis2PTLClientSecret is unset or set to the empty string"
+    exit 1
+fi
+if [ -z "${mockClientID}" ]; then
+    echo "mockClientID is unset or set to the empty string"
+    exit 1
+fi
+if [ -z "${mockClientSecret}" ]; then
+    echo "mockClientSecret is unset or set to the empty string"
+    exit 1
+fi
+
 check_gh_logged_in
 set_secrets
