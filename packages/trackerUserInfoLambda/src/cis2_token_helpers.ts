dd9828e5e6531eb0643014cee31a5ad05322a17a
Jim Wild <james.wild6@nhs.net>
2024-11-21 12:52:48 +0000

    Rework handler token stuff

--- packages/trackerUserInfoLambda/src/cis2_token_helpers.ts
+++ packages/trackerUserInfoLambda/src/cis2_token_helpers.ts
@@ -1,146 +1,93 @@
-import axios from "axios"
 import {Logger} from "@aws-lambda-powertools/logger"
-import {parse, ParsedUrlQuery, stringify} from "querystring"
-import {getSecret} from "@aws-lambda-powertools/parameters/secrets"
-import {JwtPayload, PrivateKey} from "jsonwebtoken"
-import {verifyJWTWrapper, rewriteBodyToAddSignedJWT} from "./helpers"
+import {APIGatewayProxyEvent} from "aws-lambda"
+import axios from "axios"
+import {DynamoDBDocumentClient, GetCommand} from "@aws-sdk/lib-dynamodb"
 
-const axiosInstance = axios.create()
+export const fetchAndVerifyCIS2Tokens = async (
+  event: APIGatewayProxyEvent,
+  documentClient: DynamoDBDocumentClient,
+  logger: Logger
+) => {
 
-interface FetchAndVerifyTokensParams {
-  logger: Logger;
-  body: string;
-  useSignedJWT: string;
-  jwtPrivateKeyArn: string;
-  idpTokenPath: string;
-  oidcIssuer: string;
-  oidcClientId: string;
-}
+  logger.info("Fetching and verifying CIS2 tokens")
 
-export async function fetchAndVerifyCIS2Tokens(params: FetchAndVerifyTokensParams) {
-  const {
-    logger,
-    body,
-    useSignedJWT,
-    jwtPrivateKeyArn,
-    idpTokenPath,
-    oidcIssuer,
-    oidcClientId
-  } = params
-
-  const objectBodyParameters = parse(body)
-  logger.debug("Parsed request body parameters", {objectBodyParameters})
-
-  let rewrittenObjectBodyParameters: ParsedUrlQuery
-
-  if (useSignedJWT === "true") {
-    try {
-      logger.info("Fetching JWT private key")
-      const jwtPrivateKey = await getSecret(jwtPrivateKeyArn)
-      rewrittenObjectBodyParameters = rewriteBodyToAddSignedJWT(
-        logger, objectBodyParameters, idpTokenPath, jwtPrivateKey as PrivateKey
-      )
-      logger.debug("Rewritten body with signed JWT", {rewrittenObjectBodyParameters})
-    } catch (error) {
-      logger.error("Error fetching or processing JWT private key", {error})
-      throw error
-    }
-  } else {
-    rewrittenObjectBodyParameters = objectBodyParameters
-    logger.info("JWT signing is not enabled, using original body parameters")
+  const TokenMappingTableName = process.env["TokenMappingTableName"]
+  if (!TokenMappingTableName) {
+    throw new Error("Token mapping table name not set")
   }
 
-  logger.debug("Calling downstream IDP with rewritten body", {idpTokenPath, body: rewrittenObjectBodyParameters})
+  // Extract tokens from request headers
+  const authorizationHeader = event.headers["Authorization"] || event.headers["authorization"]
+  if (!authorizationHeader) {
+    throw new Error("Authorization header missing")
+  }
 
+  // Extract the idToken from the Authorization header
+  const idToken = authorizationHeader.replace("Bearer ", "").trim()
+
+  // Decode the idToken, which is a JWT. We need the `cognito:username` claim to fetch the user data from DynamoDB
+  const username = event.requestContext.authorizer?.claims["cognito:username"]
+  if (!username){
+    throw new Error("Unable to extract username from ID token")
+  }
+  logger.info("Extracted username from ID token", {username})
+
+  // Fetch the relevant document from DynamoDB, containing the CIS2 tokens
+  let cis2AccessToken
+  let cis2IdToken
+  let existingData
   try {
-    const tokenResponse = await axiosInstance.post(idpTokenPath, stringify(rewrittenObjectBodyParameters))
-    logger.debug("Response from external OIDC", {data: tokenResponse.data})
+    logger.info("Fetching CIS2 access token from DynamoDB")
+    const result = await documentClient.send(
+      new GetCommand({
+        TableName: TokenMappingTableName,
+        Key: {username}
+      })
+    )
+    logger.info("DynamoDB response", {result})
 
-    const accessToken = tokenResponse.data.access_token
-    const idToken = tokenResponse.data.id_token
-
-    // Ensure tokens exist before proceeding
-    if (!accessToken || !idToken) {
-      logger.error("Failed to retrieve tokens from OIDC response")
-      throw new Error("Failed to retrieve tokens from OIDC response")
-    }
-
-    // Verify and decode idToken
-    logger.info("Verifying and decoding ID token")
-    const decodedIdToken = await verifyJWTWrapper(idToken, oidcIssuer, oidcClientId)
-    logger.debug("Decoded idToken", {decodedIdToken})
-
-    return {
-      accessToken,
-      idToken,
-      decodedIdToken
+    if (result.Item) {
+      existingData = result.Item
+      cis2AccessToken = existingData.CIS2_accessToken
+      cis2IdToken = existingData.CIS2_idToken
+    } else {
+      logger.error("CIS2 access token not found for user")
+      throw new Error("CIS2 access token not found for user")
     }
   } catch (error) {
-    if (error instanceof Error) {
-      logger.error("Error occurred while fetching or verifying tokens", {error: error.message})
-    } else {
-      logger.error("Unknown error occurred while fetching or verifying tokens", {error: String(error)})
-    }
-    throw error
+    logger.error("Error fetching data from DynamoDB", {error})
+    throw new Error("Internal server error while accessing DynamoDB")
+  }
+
+  verifyIdToken(idToken)
+
+  return {cis2AccessToken, cis2IdToken}
+}
+
+// TODO: Verify the token with CIS2
+const verifyIdToken = async (idToken: string) => {
+  const oidcIssuer = process.env["oidcIssuer"]
+  if (!oidcIssuer) {
+    throw new Error("OIDC issuer not set")
+  }
+
+  if (!idToken) {
+    throw new Error("ID token not provided")
   }
 }
 
-interface FetchUserInfoParams {
-  logger: Logger;
-  accessToken: string;
-  decodedIdToken: JwtPayload;
-  oidcUserInfoEndpoint: string;
-}
-
-export async function fetchUserInfo(params: FetchUserInfoParams) {
-  const {
-    logger,
-    accessToken,
-    decodedIdToken,
-    oidcUserInfoEndpoint
-  } = params
-
-  try {
-    logger.info("Sending UserInfo request", {oidcUserInfoEndpoint})
-
-    const userInfoResponse = await axiosInstance.get(oidcUserInfoEndpoint, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }
-    })
-
-    logger.debug("UserInfo response data", {data: userInfoResponse.data})
-
-    // Verify that the 'sub' claim matches the one in the ID Token
-    const userInfoSub = userInfoResponse.data.sub
-    const idTokenSub = decodedIdToken.sub
-
-    if (userInfoSub !== idTokenSub) {
-      logger.error("The 'sub' claim in UserInfo response does not match the 'sub' in ID Token", {
-        userInfoSub,
-        idTokenSub
-      })
-      throw new Error("The 'sub' claim does not match between UserInfo response and ID Token")
-    }
-
-    return userInfoResponse.data
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      logger.error("Axios error occurred while fetching UserInfo", {
-        error: error.message,
-        responseData: error.response?.data
-      })
-      throw new Error(`UserInfo request failed: ${error.message}`)
-    } else if (error instanceof Error) {
-      logger.error("Error occurred while fetching UserInfo", {
-        error: error.message
-      })
-      throw error
-    } else {
-      logger.error("Unknown error occurred while fetching UserInfo", {
-        error: String(error)
-      })
-      throw new Error("Unknown error occurred while fetching UserInfo")
-    }
+export const fetchUserInfo = async (accessToken: string, logger: Logger) => {
+  const oidcUserInfoEndpoint = process.env["oidcUserInfoEndpoint"]
+  if (!oidcUserInfoEndpoint) {
+    throw new Error("OIDC UserInfo endpoint not set")
   }
+  logger.info("Fetching user info from OIDC UserInfo endpoint", {oidcUserInfoEndpoint})
+
+  const response = await axios.get(oidcUserInfoEndpoint, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  })
+
+  return response.data
 }
--- packages/trackerUserInfoLambda/src/handler.ts
+++ packages/trackerUserInfoLambda/src/handler.ts
@@ -5,18 +5,10 @@ import middy from "@middy/core"
 import inputOutputLogger from "@middy/input-output-logger"
 import {MiddyErrorHandler} from "@cpt-ui-common/middyErrorHandler"
 import {DynamoDBClient} from "@aws-sdk/client-dynamodb"
-import {DynamoDBDocumentClient, PutCommand} from "@aws-sdk/lib-dynamodb"
+import {DynamoDBDocumentClient} from "@aws-sdk/lib-dynamodb"
 import {fetchAndVerifyCIS2Tokens, fetchUserInfo} from "./cis2_token_helpers"
 
 const logger = new Logger({serviceName: "trackerUserInfo"})
-const UserPoolIdentityProvider = process.env["UserPoolIdentityProvider"] as string
-const idpTokenPath = process.env["idpTokenPath"] as string
-const TokenMappingTableName = process.env["TokenMappingTableName"] as string
-const useSignedJWT = process.env["useSignedJWT"] as string
-const jwtPrivateKeyArn = process.env["jwtPrivateKeyArn"] as string
-const oidcClientId = process.env["oidcClientId"] as string
-const oidcIssuer = process.env["oidcIssuer"] as string
-const oidcUserInfoEndpoint = process.env["oidcUserInfoEndpoint"] as string
 
 const dynamoClient = new DynamoDBClient({})
 const documentClient = DynamoDBDocumentClient.from(dynamoClient)
@@ -31,57 +23,23 @@ const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPro
   logger.appendKeys({"apigw-request-id": event.requestContext?.requestId})
   logger.info("Lambda handler invoked", {event})
 
-  const body = event.body
-  if (body === null || body === undefined) {
-    logger.error("Request body is missing")
-    throw new Error("Request body is missing")
-  }
-
   try {
-    const {accessToken, idToken, decodedIdToken} = await fetchAndVerifyCIS2Tokens({
-      logger,
-      body,
-      useSignedJWT,
-      jwtPrivateKeyArn,
-      idpTokenPath,
-      oidcIssuer,
-      oidcClientId
-    })
-
-    const username = `${UserPoolIdentityProvider}_${decodedIdToken.sub}`
-    const params = {
-      Item: {
-        username,
-        accessToken,
-        idToken,
-        expiresIn: decodedIdToken.exp
-      },
-      TableName: TokenMappingTableName
-    }
-
-    logger.debug("Inserting into DynamoDB", {params})
-    await documentClient.send(new PutCommand(params))
+    // Fetch and verify CIS2 tokens
+    const {cis2AccessToken, cis2IdToken} = await fetchAndVerifyCIS2Tokens(event, documentClient, logger)
+    logger.info("CIS2 tokens fetched and verified", {cis2AccessToken, cis2IdToken})
 
     logger.info("Making UserInfo request")
-    const userInfoResponse = await fetchUserInfo({
-      logger,
-      accessToken,
-      decodedIdToken,
-      oidcUserInfoEndpoint
-    })
+    const userInfoResponse = await fetchUserInfo(cis2AccessToken, logger)
 
     logger.info("UserInfo response received", {userInfoResponse})
 
     return {
       statusCode: 200,
-      body: JSON.stringify(
-        {
-          message: "UserInfo fetched successfully",
-          userInfo: userInfoResponse
-        }
-      )
+      body: JSON.stringify({
+        message: "UserInfo fetched successfully",
+        userInfo: userInfoResponse
+      })
     }
-
   } catch (error) {
     if (error instanceof Error) {
       logger.error("Error occurred in Lambda handler", {error: error.message})
