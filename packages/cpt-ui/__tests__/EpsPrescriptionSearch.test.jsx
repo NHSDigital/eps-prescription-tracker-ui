66dbe1eca10b98caeead986ee3f99fb6b2a935ff
tobytobytoby74 <160478108+tobytobytoby74@users.noreply.github.com>
2024-11-29 13:53:51 +0000

    test update

--- packages/cpt-ui/__tests__/EpsPrescriptionSearch.test.jsx
+++ packages/cpt-ui/__tests__/EpsPrescriptionSearch.test.jsx
@@ -8,7 +8,33 @@ import {
 
 describe('The tabs component', () => {
 
+  it('Matches the snapshot', () => {
+    const { container } = render(
+      <Tabs>
+        <Tabs.Title>Contents</Tabs.Title>
+        <Tabs.List>
+          <Tabs.ListItem id="past-day">Past day</Tabs.ListItem>
+          <Tabs.ListItem id="past-week">Past week</Tabs.ListItem>
+          <Tabs.ListItem id="past-month">Past month</Tabs.ListItem>
+        </Tabs.List>
 
+        <Tabs.Contents id="past-day">
+          <div>Past day contents go here</div>
+        </Tabs.Contents>
+
+        <Tabs.Contents id="past-week">
+          <div>Past week contents go here</div>
+        </Tabs.Contents>
+
+        <Tabs.Contents id="past-month">
+          <div>Past month contents go here</div>
+        </Tabs.Contents>
+      </Tabs>,
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+  
   it('Switches the visibility of tabs when clicked', () => {
     const tabData = PRESCRIPTION_SEARCH_TABS;
     const { container } = render(
