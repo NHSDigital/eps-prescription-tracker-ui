132fade0d0223ab5bc14016c1e7f2315266eb2d3
tobytobytoby74 <160478108+tobytobytoby74@users.noreply.github.com>
2024-12-06 09:12:14 +0000

    REMOVES PLACEHOLDER DUPE CODE

--- packages/cpt-ui/app/confirmrole/page.tsx
+++ packages/cpt-ui/app/confirmrole/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React from "react";
 
-import { Button, Card, Container, Col, InsetText, Row, Tabs } from "nhsuk-react-components";
+import { Button, Card, Container, Col, InsetText, Row } from "nhsuk-react-components";
 export default function Page() {
     return (
         <main className="nhsuk-main-wrapper">
@@ -11,11 +11,8 @@ export default function Page() {
                         <Row>
                             <Col width="full">
                                 <h1 className='nhsuk-heading-xl '>
-                                    <span role="text">Select your role
-                                        <span className="nhsuk-caption-l nhsuk-caption--bottom">
-                                            <span className="nhsuk-u-visually-hidden"> - </span>
-                                            Select the role you wish to use to access the service.
-                                        </span></span></h1>
+                                    <span role="text">Hello World</span></h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam posuere semper libero, nec vehicula turpis varius vitae. Donec sodales ullamcorper fringilla. Donec ultrices neque et convallis venenatis. Morbi consequat, diam quis efficitur condimentum, augue quam finibus orci, et ornare sapien odio vitae lorem. Phasellus iaculis sagittis lacus, eget commodo dolor feugiat in. Vivamus facilisis ligula quis mattis ultricies. Ut luctus neque et maximus fringilla. Pellentesque venenatis augue eu lacus finibus luctus. Integer vel nulla risus. Suspendisse in cursus ante, at auctor arcu. Nunc hendrerit nec nisi non venenatis. Mauris aliquet convallis pharetra. Mauris ac velit arcu. Vivamus eget metus sed sapien tempor tristique ut sit amet magna. Mauris interdum at nunc non imperdiet. Proin dictum tempor nunc quis sollicitudin.</p>
                             </Col>
                         </Row>
                         <Row>
@@ -44,28 +41,7 @@ export default function Page() {
                                 </Card>
                             </Col>
                         </Row>
-                        <Row>
-                            <Tabs>
-                                <Tabs.Title>Contents</Tabs.Title>
-                                <Tabs.List>
-                                    <Tabs.ListItem id="past-day">Past day</Tabs.ListItem>
-                                    <Tabs.ListItem id="past-week">Past week</Tabs.ListItem>
-                                    <Tabs.ListItem id="past-month">Past month</Tabs.ListItem>
-                                </Tabs.List>
 
-                                <Tabs.Contents id="past-day">
-                                    <div>Past day contents go here</div>
-                                </Tabs.Contents>
-
-                                <Tabs.Contents id="past-week">
-                                    <div>Past week contents go here</div>
-                                </Tabs.Contents>
-
-                                <Tabs.Contents id="past-month">
-                                    <div>Past month contents go here</div>
-                                </Tabs.Contents>
-                            </Tabs>
-                        </Row>
                     </Container>
                 </Row>
             </Container>
