6f51f5ca7823c87993a3f6216f05a0c7fa9a307b
Kris Szlapa <kris.szlapa1@nhs.net>
2024-12-09 15:08:31 +0000

    Add CardStrings and update EpsCard file

---
+++ packages/cpt-ui/constants/ui-strings/CardStrings.ts
@@ -0,0 +1,14 @@
+export const ROLE_CARDS = [
+  {
+    name: "JONES SURGERY",
+    odsCode: "RCD60",
+    address: "4 Sardinia street\nHolborn\nLondon\nSE4 6ER",
+    specialty: "General Medical Practitioner"
+  },
+  {
+    name: "JONES SURGERY",
+    odsCode: "RCD60",
+    address: null, // No address
+    specialty: "General Medical Practitioner"
+  }
+]
