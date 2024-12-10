fd2e4d0acad482131df449cf416e28de0cb855e8
Jim Wild <james.wild6@nhs.net>
2024-12-10 16:44:34 +0000

    Basic test

---
+++ packages/trackerUserInfoLambda/tests/mockObjects.ts
@@ -0,0 +1,79 @@
+export const mockAPIGatewayProxyEvent = {
+  httpMethod: "POST",
+  body: "",
+  headers: {
+    "nhsd-nhslogin-user": "P9:9912003071",
+    "nhsd-correlation-id": "test-request-id.test-correlation-id.rrt-5789322914740101037-b-aet2-20145-482635-2",
+    "x-request-id": "test-request-id",
+    "nhsd-request-id": "test-request-id",
+    "x-correlation-id": "test-correlation-id"
+  },
+  isBase64Encoded: false,
+  multiValueHeaders: {},
+  multiValueQueryStringParameters: {},
+  path: "/hello",
+  pathParameters: {},
+  queryStringParameters: {},
+  requestContext: {
+    accountId: "123456789012",
+    apiId: "1234",
+    authorizer: {
+      claims: {
+        "cognito:username": "Mock_JoeBloggs"
+      }
+    },
+    httpMethod: "POST",
+    identity: {
+      accessKey: "",
+      accountId: "",
+      apiKey: "",
+      apiKeyId: "",
+      caller: "",
+      clientCert: {
+        clientCertPem: "",
+        issuerDN: "",
+        serialNumber: "",
+        subjectDN: "",
+        validity: {
+          notAfter: "",
+          notBefore: ""
+        }
+      },
+      cognitoAuthenticationProvider: "",
+      cognitoAuthenticationType: "",
+      cognitoIdentityId: "",
+      cognitoIdentityPoolId: "",
+      principalOrgId: "",
+      sourceIp: "",
+      user: "",
+      userAgent: "",
+      userArn: ""
+    },
+    path: "/",
+    protocol: "HTTP/1.1",
+    requestId: "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
+    requestTimeEpoch: 1428582896000,
+    resourceId: "123456",
+    resourcePath: "/",
+    stage: "dev"
+  },
+  resource: "",
+  stageVariables: {}
+}
+
+
+export const mockContext = {
+  callbackWaitsForEmptyEventLoop: true,
+  functionVersion: "$LATEST",
+  functionName: "foo-bar-function",
+  memoryLimitInMB: "128",
+  logGroupName: "/aws/lambda/foo-bar-function-123456abcdef",
+  logStreamName: "2021/03/09/[$LATEST]abcdef123456abcdef123456abcdef123456",
+  invokedFunctionArn:
+    "arn:aws:lambda:eu-west-1:123456789012:function:foo-bar-function",
+  awsRequestId: "c6af9ac6-7b61-11e6-9a41-93e812345678",
+  getRemainingTimeInMillis: () => 1234,
+  done: () => console.log("Done!"),
+  fail: () => console.log("Failed!"),
+  succeed: () => console.log("Succeeded!")
+}
