131717560684711b41690dbfbda1ec34d00179c9
tobytobytoby74 <160478108+tobytobytoby74@users.noreply.github.com>
2024-12-11 10:12:20 +0000

    break the test

--- packages/cpt-ui/next.config.js
+++ packages/cpt-ui/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   output: process.env.NEXT_OUTPUT_MODE || undefined,
   basePath: process.env.BASE_PATH || ""
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
+  // trailingSlash: true
 
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
   // skipTrailingSlashRedirect: true,
