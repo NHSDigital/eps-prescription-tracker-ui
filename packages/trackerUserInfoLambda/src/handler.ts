099ed7dacc7eb260f7428ad68e25999d0d19f470
Jim Wild <james.wild6@nhs.net>
2024-12-11 10:35:45 +0000

    linting

--- packages/trackerUserInfoLambda/src/handler.ts
+++ packages/trackerUserInfoLambda/src/handler.ts
@@ -37,23 +37,37 @@ const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPro
   const isMockRequest = MOCK_MODE_ENABLED === "true" && isMockToken
 
   // set environment variables
-  process.env.idpTokenPath = isMockRequest ? process.env.MOCK_IDP_TOKEN_PATH : process.env.REAL_IDP_TOKEN_PATH
-  // eslint-disable-next-line max-len
-  process.env.userInfoEndpoint = isMockRequest ? process.env.MOCK_USER_INFO_ENDPOINT : process.env.REAL_USER_INFO_ENDPOINT
-  process.env.oidcjwksEndpoint = isMockRequest ? process.env.MOCK_OIDCJWKS_ENDPOINT : process.env.REAL_OIDCJWKS_ENDPOINT
-  // eslint-disable-next-line max-len
-  process.env.jwtPrivateKeyArn = isMockRequest ? process.env.MOCK_JWT_PRIVATE_KEY_ARN : process.env.REAL_JWT_PRIVATE_KEY_ARN
-  process.env.userPoolIdp = isMockRequest ? process.env.MOCK_USER_POOL_IDP : process.env.REAL_USER_POOL_IDP
-  process.env.useSignedJWT = isMockRequest ? process.env.MOCK_USE_SIGNED_JWT : process.env.REAL_USE_SIGNED_JWT
-  process.env.oidcClientId = isMockRequest ? process.env.MOCK_OIDC_CLIENT_ID : process.env.REAL_OIDC_CLIENT_ID
-  process.env.oidcIssuer = isMockRequest ? process.env.MOCK_OIDC_ISSUER : process.env.REAL_OIDC_ISSUER
+  process.env.idpTokenPath = isMockRequest
+    ? process.env.MOCK_IDP_TOKEN_PATH
+    : process.env.REAL_IDP_TOKEN_PATH
+
+  process.env.userInfoEndpoint = isMockRequest
+    ? process.env.MOCK_USER_INFO_ENDPOINT
+    : process.env.REAL_USER_INFO_ENDPOINT
+  process.env.oidcjwksEndpoint = isMockRequest
+    ? process.env.MOCK_OIDCJWKS_ENDPOINT
+    : process.env.REAL_OIDCJWKS_ENDPOINT
+
+  process.env.jwtPrivateKeyArn = isMockRequest
+    ? process.env.MOCK_JWT_PRIVATE_KEY_ARN
+    : process.env.REAL_JWT_PRIVATE_KEY_ARN
+  process.env.userPoolIdp = isMockRequest
+    ? process.env.MOCK_USER_POOL_IDP
+    : process.env.REAL_USER_POOL_IDP
+  process.env.useSignedJWT = isMockRequest
+    ? process.env.MOCK_USE_SIGNED_JWT
+    : process.env.REAL_USE_SIGNED_JWT
+  process.env.oidcClientId = isMockRequest
+    ? process.env.MOCK_OIDC_CLIENT_ID
+    : process.env.REAL_OIDC_CLIENT_ID
+  process.env.oidcIssuer = isMockRequest
+    ? process.env.MOCK_OIDC_ISSUER
+    : process.env.REAL_OIDC_ISSUER
 
   try {
-    // Fetch and verify CIS2 tokens
+    // eslint-disable-next-line
     const {cis2AccessToken, cis2IdToken} = await fetchAndVerifyCIS2Tokens(event, documentClient, logger)
-    logger.info("CIS2 tokens fetched and verified", {cis2AccessToken, cis2IdToken})
 
-    // logger.info("Making UserInfo request")
     const userInfoResponse = await fetchUserInfo(
       cis2AccessToken,
       CPT_ACCESS_ACTIVITY_CODES,
@@ -61,11 +75,8 @@ const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPro
       logger
     )
 
-    // logger.info("UserInfo response received", {userInfoResponse})
-
     const username = getUsernameFromEvent(event)
     updateDynamoTable(username, userInfoResponse, documentClient, logger)
-    // logger.info("Updated DynamoDB table with user info", {username})
 
     return {
       statusCode: 200,
@@ -76,10 +87,12 @@ const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPro
     }
   } catch (error) {
     if (error instanceof Error) {
+      logger.error("Lambda failed!!! [DELETEME]")
       logger.error("Error occurred in Lambda handler", {error: error.message})
     } else {
       logger.error("Unknown error occurred in Lambda handler", {error: String(error)})
     }
+    logger.info("trackerUserInfo success!")
     return {
       statusCode: 500,
       body: JSON.stringify({message: "Internal server error"})
