32087d59b1a3589b6b07f1a0cfecb574bb1727b5
Kris Szlapa <kris.szlapa1@nhs.net>
2024-12-13 16:55:43 +0000

    Set the REGRESSION_TESTS_REPO_TAG environment variable to the branch name

--- scripts/run_regression_tests.py
+++ scripts/run_regression_tests.py
@@ -12,7 +12,7 @@ import requests
 import time
 
 # This should be set to a known good version of regression test repo
-REGRESSION_TESTS_REPO_TAG = "main"
+REGRESSION_TESTS_REPO_TAG = "AEA-4651-roles-with-access"
 
 GITHUB_API_URL = "https://api.github.com/repos/NHSDigital/electronic-prescription-service-api-regression-tests/actions"
 
