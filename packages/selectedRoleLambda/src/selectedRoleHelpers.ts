4b5fd16e259244ea459f249deb3f2cd835470994
Kris Szlapa <kris.szlapa1@nhs.net>
2025-01-22 17:12:21 +0000

    Use mock data for testing

--- packages/selectedRoleLambda/src/selectedRoleHelpers.ts
+++ packages/selectedRoleLambda/src/selectedRoleHelpers.ts
@@ -2,6 +2,38 @@ import {Logger} from "@aws-lambda-powertools/logger"
 import {DynamoDBDocumentClient, UpdateCommand} from "@aws-sdk/lib-dynamodb"
 import {RoleDetails} from "./selectedRoleTypes"
 
+// Mock data for rolesWithAccess and rolesWithoutAccess
+const rolesWithAccess: Array<RoleDetails> = [
+  {
+    role_name: "General Medical Practitioner",
+    role_id: "555043301111",
+    org_code: "C82024"
+  },
+  {
+    role_name: "General Medical Practitioner",
+    role_id: "555043302222",
+    org_code: "P92602"
+  }
+]
+
+const rolesWithoutAccess: Array<RoleDetails> = [
+  {
+    role_name: "General Medical Practitioner",
+    role_id: "555043303333",
+    org_code: "A81060"
+  },
+  {
+    role_name: "Consultant",
+    role_id: "555043304444",
+    org_code: "A20063"
+  },
+  {
+    role_name: "General Medical Practitioner",
+    role_id: "555043305555",
+    org_code: "A21292"
+  }
+]
+
 // Add the user currentlySelectedRole to the DynamoDB document, keyed by this user's username.
 export const updateDynamoTable = async (
   username: string,
@@ -15,54 +47,80 @@ export const updateDynamoTable = async (
     throw new Error("Token mapping table name not set")
   }
 
+  if (tokenMappingTableName !== "cpt-ui-pr-334-stateful-resources-TokenMapping") {
+    logger.error("Incorrect token mapping table name", {tokenMappingTableName})
+    throw new Error("Incorrect token mapping table name")
+  }
+
+  if (username !== "Mock_555043304334") {
+    logger.error("Incorrect username", {username})
+    throw new Error("Incorrect username")
+  }
+
   logger.debug("Starting DynamoDB update process", {
     username,
     table: tokenMappingTableName,
     receivedData: data
   })
 
-  // Construct the currentlySelectedRole object from the provided data
-  const currentlySelectedRole = {
-    role_id: data.role_id ?? "",
-    org_code: data.org_code ?? "",
-    role_name: data.role_name ?? ""
-  }
+  // Construct the currentlySelectedRole object from the provided data, excluding empty values
+  const currentlySelectedRole: RoleDetails = Object.fromEntries(
+    Object.entries({
+      role_id: data.role_id ?? "",
+      org_code: data.org_code ?? "",
+      role_name: data.role_name ?? "",
+      org_name: data.org_name ?? ""
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    }).filter(([_, value]) => value !== "") // Remove keys with empty string values
+  )
 
   const selectedRoleId: string = data.role_id ?? ""
 
-  // Remove undefined values to prevent errors in DynamoDB update
-  const scrubbedCurrentlySelectedRole = JSON.parse(
-    JSON.stringify(currentlySelectedRole)
-  )
-
   logger.info("Prepared data for DynamoDB update", {
-    currentlySelectedRole: scrubbedCurrentlySelectedRole,
+    currentlySelectedRole,
     selectedRoleId
   })
 
   try {
+    logger.info("Preparing to send update command to DynamoDB", {
+      TableName: tokenMappingTableName,
+      Key: {username},
+      UpdateExpression:
+        "SET rolesWithAccess = :rolesWithAccess, " +
+        "rolesWithoutAccess = :rolesWithoutAccess, " +
+        "currentlySelectedRole = :currentlySelectedRole, " +
+        "selectedRoleId = :selectedRoleId",
+      ExpressionAttributeValues: {
+        ":rolesWithAccess": rolesWithAccess,
+        ":rolesWithoutAccess": rolesWithoutAccess,
+        ":currentlySelectedRole": currentlySelectedRole,
+        ":selectedRoleId": selectedRoleId
+      }
+    })
+
     await documentClient.send(
       new UpdateCommand({
         TableName: tokenMappingTableName,
         Key: {username},
         UpdateExpression:
-          "SET currentlySelectedRole.role_id = :role_id, " +
-          "currentlySelectedRole.org_code = :org_code, " +
-          "currentlySelectedRole.role_name = :role_name, " +
+          "SET rolesWithAccess = :rolesWithAccess, " +
+          "rolesWithoutAccess = :rolesWithoutAccess, " +
+          "currentlySelectedRole = :currentlySelectedRole, " +
           "selectedRoleId = :selectedRoleId",
         ExpressionAttributeValues: {
-          ":role_id": scrubbedCurrentlySelectedRole.role_id,
-          ":org_code": scrubbedCurrentlySelectedRole.org_code,
-          ":role_name": scrubbedCurrentlySelectedRole.role_name,
+          ":rolesWithAccess": rolesWithAccess,
+          ":rolesWithoutAccess": rolesWithoutAccess,
+          ":currentlySelectedRole": currentlySelectedRole,
           ":selectedRoleId": selectedRoleId
         },
-        ReturnValues: "UPDATED_NEW" // Returns only the attributes that were updated in the response
+        ReturnValues: "UPDATED_NEW"
       })
     )
 
     logger.info("DynamoDB update successful", {
       username,
-      updatedRole: scrubbedCurrentlySelectedRole
+      updatedRole: currentlySelectedRole,
+      selectedRoleId: selectedRoleId
     })
   } catch (error) {
     if (error instanceof Error) {
