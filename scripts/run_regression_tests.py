de3fdd60d9a609bf45a3345e28f65fe1a5850049
Jim Wild <james.wild6@nhs.net>
2025-01-23 16:28:31 +0000

    Point to test regression tests

--- scripts/run_regression_tests.py
+++ scripts/run_regression_tests.py
@@ -13,7 +13,7 @@ import time
 from requests.auth import HTTPBasicAuth
 
 # This should be set to a known good version of regression test repo
-REGRESSION_TESTS_REPO_TAG = "v2.9.0"
+REGRESSION_TESTS_REPO_TAG = "aea-0000-axios-migrate"
 
 GITHUB_API_URL = "https://api.github.com/repos/NHSDigital/electronic-prescription-service-api-regression-tests/actions"
 GITHUB_RUN_URL = "https://github.com/NHSDigital/electronic-prescription-service-api-regression-tests/actions/runs"
