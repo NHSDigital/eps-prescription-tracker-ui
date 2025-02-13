94d601bb3a2767ffc21680ecd23f9b58ba95dfe3
Kris Szlapa <kris.szlapa1@nhs.net>
2025-02-13 13:19:22 +0000

    Implement the middyErrorHandler in the prescriptionDetail lambda handler

--- packages/prescriptionDetailsLambda/src/handler.ts
+++ packages/prescriptionDetailsLambda/src/handler.ts
@@ -1,6 +1,15 @@
 import {APIGatewayProxyHandler} from "aws-lambda"
+import {Logger} from "@aws-lambda-powertools/logger"
+import {injectLambdaContext} from "@aws-lambda-powertools/logger/middleware"
+import middy from "@middy/core"
+import inputOutputLogger from "@middy/input-output-logger"
+import {MiddyErrorHandler} from "@cpt-ui-common/middyErrorHandler"
 
-export const handler: APIGatewayProxyHandler = async () => {
+const logger = new Logger({serviceName: "prescriptionDetails"})
+const errorResponseBody = {message: "A system error has occurred"}
+const middyErrorHandler = new MiddyErrorHandler(errorResponseBody)
+
+export const lambdaHandler: APIGatewayProxyHandler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
@@ -8,3 +17,14 @@ export const handler: APIGatewayProxyHandler = async () => {
     })
   }
 }
+
+export const handler = middy(lambdaHandler)
+  .use(injectLambdaContext(logger, {clearState: true}))
+  .use(
+    inputOutputLogger({
+      logger: (request) => {
+        logger.info(request)
+      }
+    })
+  )
+  .use(middyErrorHandler.errorHandler({logger: logger}))
