a946620178ccceb831a1d9f5d0936d26f10b53b6
Jim Wild <james.wild6@nhs.net>
2025-02-14 11:52:53 +0000

    Move period out of link text

--- packages/cpt-ui/src/pages/NotFoundPage.tsx
+++ packages/cpt-ui/src/pages/NotFoundPage.tsx
@@ -11,12 +11,14 @@ export default function NotFoundPage() {
     const [body2, setBody2] = useState("")
     const [body3, setBody3] = useState("")
     const [body3Link, setBody3Link] = useState("")
+    const [body3PostLink, setBody3PostLink] = useState("")
 
     useEffect(() => {
         setHeaderText(NOT_FOUND_PAGE_STRINGS.headerText)
         setBody1(NOT_FOUND_PAGE_STRINGS.bodyText1)
         setBody2(NOT_FOUND_PAGE_STRINGS.bodyText2)
         setBody3(NOT_FOUND_PAGE_STRINGS.bodyText3)
+        setBody3PostLink(NOT_FOUND_PAGE_STRINGS.bodyText3PostLink)
         setBody3Link(NOT_FOUND_PAGE_STRINGS.bodyText3LinkText)
     }, [NOT_FOUND_PAGE_STRINGS])
 
@@ -26,7 +28,7 @@ export default function NotFoundPage() {
                 <h1 data-testid="eps-404-header">{headerText}</h1>
                 <p data-testid="eps-404-body1">{body1}</p>
                 <p data-testid="eps-404-body2">{body2}</p>
-                <p data-testid="eps-404-body3">{body3}<Link to="/searchforaprescription">{body3Link}</Link></p>
+                <p data-testid="eps-404-body3">{body3}<Link to="/searchforaprescription">{body3Link}</Link>{body3PostLink}</p>
             </Container>
         </main>
     )
