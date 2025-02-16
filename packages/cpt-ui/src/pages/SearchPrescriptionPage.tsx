46d5d673f228a961fbe1189d4dfbc1d26c7becd5
Kris Szlapa <kris.szlapa1@nhs.net>
2025-02-16 23:11:14 +0000

    Restore the SearchPrescriptionPage

--- packages/cpt-ui/src/pages/SearchPrescriptionPage.tsx
+++ packages/cpt-ui/src/pages/SearchPrescriptionPage.tsx
@@ -1,43 +1,9 @@
-import React, {useContext} from "react"
+import React from "react"
 import {Col, Container, Hero, Row} from "nhsuk-react-components"
 import EpsTabs from "@/components/EpsTabs"
 import {HERO_TEXT} from "@/constants/ui-strings/SearchForAPrescriptionStrings"
-import {AuthContext} from "@/context/AuthProvider"
-
-const prescriptionDetailsEndpoint = "/api/prescription-details"
 
 export default function SearchForAPrescriptionPage() {
-  // AEA-4752: Testing prescriptionDetailsLambda integration
-  const auth = useContext(AuthContext)
-
-  const handlePrescriptionDetails = async (e: React.MouseEvent) => {
-    e.preventDefault()
-
-    const prescriptionId = "C0C757-A83008-C2D93O"
-    const url = `${prescriptionDetailsEndpoint}/${prescriptionId}`
-
-    try {
-      const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${auth?.idToken}`,
-          'NHSD-Session-URID': '555254242106'
-        }
-      })
-
-      const data = await response.json()
-      console.log("Response:", data)
-
-      if (!response.ok) {
-        throw new Error('Failed to retrieve prescription details')
-      }
-
-    } catch (error) {
-      console.error('Error retrieving prescription details:', error)
-    }
-  }
-  // End AEA-4752
-
   return (
     <>
       <title>Search for a prescription</title>
@@ -57,15 +23,6 @@ export default function SearchForAPrescriptionPage() {
           <Col width="full">
             <EpsTabs />
           </Col>
-          {/* AEA-4752: Test button for prescriptionDetailsLambda */}
-          <Container className="nhsuk-width-container">
-            <Col width="full">
-              <button className="nhsuk-button" onClick={handlePrescriptionDetails}>
-                Retrieve prescription details
-              </button>
-            </Col>
-          </Container>
-          {/* End AEA-4752 */}
         </Row>
       </main>
     </>
