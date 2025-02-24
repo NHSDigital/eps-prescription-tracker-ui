1955bdbd81747715a1ce8807c2c5f1d90bbbf749
MatthewPopat-NHS <matthew.popat1@nhs.net>
2025-02-24 12:30:12 +0000

    Initial react 19 prototype

---
+++ packages/cpt-ui/src/server-lambda.tsx
@@ -0,0 +1,28 @@
+import { LambdaFunctionURLHandler } from "aws-lambda";
+import { prerenderToNodeStream } from 'react-dom/static';
+import { StaticRouter } from 'react-router';
+import App from './App';
+
+export const handler: LambdaFunctionURLHandler = async (event) => {
+    const {prelude} = await prerenderToNodeStream(
+        <App />,
+        {
+            bootstrapScripts: ['/main.js']
+        }
+    );
+    const body = await new Promise<string>((resolve, reject) => {
+        let data = '';
+        prelude.on('data', chunk => {
+          data += chunk;
+        });
+        prelude.on('end', () => resolve(data));
+        prelude.on('error', reject);
+    });
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "text/html"
+        },
+        body
+    }
+}
