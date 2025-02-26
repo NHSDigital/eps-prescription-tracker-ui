de30219f2b930141fdef8ea0fd5ecd614d7e5eb6
Kris Szlapa <kris.szlapa1@nhs.net>
2025-02-26 16:41:20 +0000

    Extract full patient details

--- packages/prescriptionDetailsLambda/src/utils/responseMapper.ts
+++ packages/prescriptionDetailsLambda/src/utils/responseMapper.ts
@@ -23,19 +23,50 @@ export const mergePrescriptionDetails = (
   const daysSupply = "Not found"
   const prescriptionPendingCancellation = prescriptionDetails?.extension?.find(ext => ext.url === "https://fhir.nhs.uk/StructureDefinition/Extension-EPS-PendingCancellations")?.extension?.find(ext => ext.url === "prescriptionPendingCancellation")?.valueBoolean || false
 
-  // Extract Patient data
+  // Extract Patient Data
   const patientDataBundle = (prescriptionDetails.contained?.filter(
     (contained) => contained.resourceType === "Patient"
   ) as Array<Resource>) || []
 
+  // Extract Full Patient Details
   const patientDetails = patientDataBundle.length > 0 ? {
-    gender: patientDataBundle[0]?.gender ?? "Not found",
-    dateOfBirth: patientDataBundle[0]?.birthDate ?? "Not found",
-    address: patientDataBundle[0]?.address?.[0]?.text ?? "Not found"
+    identifier: patientDataBundle[0]?.identifier?.[0]?.value || "Not found",
+    name: {
+      prefix: patientDataBundle[0]?.name?.[0]?.prefix?.[0] || "Not found",
+      given: patientDataBundle[0]?.name?.[0]?.given?.join(" ") || "Not found",
+      family: patientDataBundle[0]?.name?.[0]?.family || "Not found",
+      suffix: patientDataBundle[0]?.name?.[0]?.suffix?.[0] || "Not found"
+    },
+    gender: patientDataBundle[0]?.gender || "Not found",
+    birthDate: patientDataBundle[0]?.birthDate || "Not found",
+    address: {
+      line: patientDataBundle[0]?.address?.[0]?.line?.join(", ") || "Not found",
+      city: patientDataBundle[0]?.address?.[0]?.city || "Not found",
+      district: patientDataBundle[0]?.address?.[0]?.district || "Not found",
+      postalCode: patientDataBundle[0]?.address?.[0]?.postalCode || "Not found",
+      text: patientDataBundle[0]?.address?.[0]?.text || "Not found",
+      type: patientDataBundle[0]?.address?.[0]?.type || "Not found",
+      use: patientDataBundle[0]?.address?.[0]?.use || "Not found"
+    }
   } : {
+    identifier: "Not found",
+    name: {
+      prefix: "Not found",
+      given: "Not found",
+      family: "Not found",
+      suffix: "Not found"
+    },
     gender: "Not found",
-    dateOfBirth: "Not found",
-    address: "Not found"
+    birthDate: "Not found",
+    address: {
+      line: "Not found",
+      city: "Not found",
+      district: "Not found",
+      postalCode: "Not found",
+      text: "Not found",
+      type: "Not found",
+      use: "Not found"
+    }
   }
 
   // Extract prescribed items
