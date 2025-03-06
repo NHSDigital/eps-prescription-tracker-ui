d4d6341b9a5faccd521eed04506f5a8e17c96a38
Jim Wild <james.wild6@nhs.net>
2025-03-06 13:42:50 +0000

    Remove UI changes [OUT OF SCOPE] - can still use auth_demo page

--- packages/cpt-ui/src/constants/ui-strings/SearchForAPrescriptionStrings.ts
+++ packages/cpt-ui/src/constants/ui-strings/SearchForAPrescriptionStrings.ts
@@ -1,6 +1,1 @@
 export const HERO_TEXT = "Search for a prescription"
-export const PRESCRIPTION_ID_SEARCH_STRINGS = {
-  labelText: "Search using a prescription ID",
-  hintText: "This is 18 characters and may also be called barcode or token ID.",
-  buttonText: "Find a prescription"
-}
