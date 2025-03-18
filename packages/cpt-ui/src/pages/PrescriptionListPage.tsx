6d73172b5d751b627633cc1de9021d6a95087c9a
Bence Gadanyi <bence.gadanyi1@nhs.net>
2025-03-18 15:56:43 +0000

    rearranges layout

--- packages/cpt-ui/src/pages/PrescriptionListPage.tsx
+++ packages/cpt-ui/src/pages/PrescriptionListPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BackLink, Col, Container, Hero, Row } from "nhsuk-react-components";
+import { BackLink, Col, Container, Hero, Row, Header } from "nhsuk-react-components";
 import { Link } from "react-router-dom";
 
 export default function PrescriptionListPage() {
@@ -13,37 +13,31 @@ export default function PrescriptionListPage() {
                 <Container className="hero-container">
                     <Row>
                         <Col width="full">
-                            <Hero className="nhsuk-hero-wrapper" data-testid="hero-banner">
-                                <Hero.Heading className="heroHeading" id="hero-heading" data-testid="hero-heading">
-                                    Prescriptions list
-                                </Hero.Heading>
-                            </Hero>
-                        </Col>
-                    </Row>
-                </Container>
-
-                <Container>
-                    <Row>
-                        <Col width="full">
                             <Link to="/searchforaprescription">
                                 <BackLink data-testid="go-back-link">Go back</BackLink>
                             </Link>
                         </Col>
                     </Row>
-
                     <Row>
                         <Col width="full">
-                            <Hero.Text data-testid="results-heading">
-                                <p data-testid="results-count">We found {prescriptionCount} results</p>
-                            </Hero.Text>
-
-                            <div data-testid="prescription-results">
-                                {/* Prescription list items would go here see more here: https://prototype-nhs-eps.herokuapp.com/epsv12/prescription-results?nhsNumber=9726919215#current-prescriptions */}
-                            </div>
+                            {/* <Hero className="nhsuk-hero-wrapper" data-testid="hero-banner"> */}
+                            <h2 className="nhsuk-heading-l" id="hero-heading" data-testid="hero-heading">
+                                Prescriptions list
+                            </h2>
+                            {/* </Hero> */}
                         </Col>
                     </Row>
                 </Container>
-            </main>
+                <Container className="results-container">
+                    <p data-testid="results-heading">
+                        <strong data-testid="results-count">We found {prescriptionCount} results</strong>
+                    </p>
+                    <div data-testid="prescription-results-list">
+                        {/* Prescription list items would go here see more here: https://prototype-nhs-eps.herokuapp.com/epsv12/prescription-results?nhsNumber=9726919215#current-prescriptions */}
+                    </div>
+                </Container>
+
+            </main >
         </>
     );
 }
