7ad6b7aa1bfcd23a5b8e4f4346619e1fd6c31420
Bence Gadanyi <bence.gadanyi1@nhs.net>
2025-03-18 14:38:23 +0000

    post-rebase fix

--- packages/cdk/nagSuppressions.ts
+++ packages/cdk/nagSuppressions.ts
@@ -178,7 +178,7 @@ export const nagSuppressions = (stack: Stack) => {
 
     safeAddNagSuppression(
       stack,
-      "/StatelessStack/ApiFunctions/PrescriptionSearch/LambdaPutLogsManagedPolicy/Resource",
+      "/StatelessStack/ApiFunctions/PrescriptionList/LambdaPutLogsManagedPolicy/Resource",
       [
         {
           id: "AwsSolutions-IAM5",
