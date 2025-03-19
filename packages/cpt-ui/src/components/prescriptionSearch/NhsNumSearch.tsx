842aa38ea249d6097f1297bbb7a927ae71fc9cf6
Bence Gadanyi <bence.gadanyi1@nhs.net>
2025-03-19 09:04:56 +0000

    styling + go back button logix

--- packages/cpt-ui/src/components/prescriptionSearch/NhsNumSearch.tsx
+++ packages/cpt-ui/src/components/prescriptionSearch/NhsNumSearch.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Button } from '../ReactRouterButton';
 
 export default function NhsNumSearch() {
     return (
-        <h1>NHS Number Search</h1>
+        <>
+            <h1>NHS Number Search</h1>
+            <div className="eps-modal-button-group">
+                <Button
+                    className="nhsuk-button eps-modal-button"
+                    to="/prescriptionresults?nhsNumber=123456"
+                >
+                    Find a patient
+                </Button>
+            </div>
+        </>
     )
 }
--- packages/cpt-ui/src/components/prescriptionSearch/PrescriptionIdSearch.tsx
+++ packages/cpt-ui/src/components/prescriptionSearch/PrescriptionIdSearch.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Button } from '../ReactRouterButton';
 
 export default function PrescriptionIdSearch() {
     return (
-        <h1>Prescription ID Search</h1>
+        <>
+            <h1>Prescription ID Search</h1>
+            <div className="eps-modal-button-group">
+                <Button
+                    className="nhsuk-button eps-modal-button"
+                    to="/prescriptionresults?prescriptionId=123456"
+                >
+                    Find a prescription
+                </Button>
+            </div>
+        </>
     )
 }
