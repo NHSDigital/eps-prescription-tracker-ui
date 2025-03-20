17711cafab2569f55251168f275f1dfed6f53cc2
Jim Wild <james.wild6@nhs.net>
2025-03-20 15:57:27 +0000

    Revert search page temporary changes

--- packages/cpt-ui/src/pages/SearchPrescriptionPage.tsx
+++ packages/cpt-ui/src/pages/SearchPrescriptionPage.tsx
@@ -1,54 +1,10 @@
 import React from "react";
-import { Col, Container, Hero, Row, Button } from "nhsuk-react-components";
+import { Col, Container, Hero, Row } from "nhsuk-react-components";
 import EpsTabs from "@/components/EpsTabs";
 import { HERO_TEXT } from "@/constants/ui-strings/SearchForAPrescriptionStrings"
 
-import { useAccess } from "@/context/AccessProvider";
-
-import { PatientDetails } from "@cpt-ui-common/common-types";
 
 export default function SearchForAPrescriptionPage() {
-  const { setPatientDetails } = useAccess()
-
-  // FIXME: These need to be deleted when we start getting live data
-  const handleFullClick = () => {
-    const completeDetails: PatientDetails = {
-      nhsNumber: "5900009890",
-      prefix: "Mr",
-      suffix: "",
-      given: "William",
-      family: "Wolderton",
-      gender: "male",
-      dateOfBirth: "01-Nov-1988",
-      address: {
-        line1: "55 OAK STREET",
-        line2: "OAK LANE",
-        city: "Leeds",
-        postcode: "LS1 1XX"
-      }
-    }
-
-    setPatientDetails(completeDetails)
-  }
-
-  const handlePartialClick = () => {
-    const minimumDetails: PatientDetails = {
-      nhsNumber: "5900009890",
-      prefix: "Mr",
-      suffix: "",
-      given: "William",
-      family: "Wolderton",
-      gender: null,
-      dateOfBirth: null,
-      address: null
-    }
-    setPatientDetails(minimumDetails)
-  }
-
-  const handleClearClick = () => {
-    setPatientDetails(undefined)
-  }
-
   return (
     <>
       <title>Search for a prescription</title>
@@ -65,9 +21,6 @@ export default function SearchForAPrescriptionPage() {
           </Row>
         </Container>
         <Row>
-          <Button onClick={handleFullClick}>Complete fake patient data</Button>
-          <Button onClick={handlePartialClick}>Partial fake patient data</Button>
-          <Button onClick={handleClearClick}>Clear fake patient data</Button>
           <Col width="full">
             <EpsTabs />
           </Col>
