a2d8b071edc76c0ee6f7f2afc32c4ba256517445
Kris Szlapa <kris.szlapa1@nhs.net>
2025-03-21 08:36:14 +0000

    Add the error messages to the PRESCRIPTION_ID_SEARCH_STRINGS

--- packages/cpt-ui/src/constants/ui-strings/SearchForAPrescriptionStrings.ts
+++ packages/cpt-ui/src/constants/ui-strings/SearchForAPrescriptionStrings.ts
@@ -1,6 +1,13 @@
 export const HERO_TEXT = "Search for a prescription"
+
 export const PRESCRIPTION_ID_SEARCH_STRINGS = {
   labelText: "Search using a prescription ID",
   hintText: "This is 18 characters and may also be called barcode or token ID.",
-  buttonText: "Find a prescription"
+  buttonText: "Find a prescription",
+  errors: {
+    empty: "Enter a prescription ID number",
+    length: "The prescription ID number must contain 18 characters",
+    chars: "The prescription ID number must contain only letters, numbers, dashes or the + character",
+    noMatch: "The prescription ID number is not recognised"
+  }
 }
