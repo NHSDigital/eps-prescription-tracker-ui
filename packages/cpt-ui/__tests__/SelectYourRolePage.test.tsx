f62ff7752bc0b262596bea659a645d5da90a23f3
Bence Gadanyi <bence.gadanyi1@nhs.net>
2025-03-21 16:05:08 +0000

    chore: updates leftover link target changes

--- packages/cpt-ui/__tests__/SelectYourRolePage.test.tsx
+++ packages/cpt-ui/__tests__/SelectYourRolePage.test.tsx
@@ -27,7 +27,7 @@ jest.mock("@/constants/ui-strings/CardStrings", () => {
     },
     confirmButton: {
       text: "Continue to find a prescription",
-      link: "searchforaprescription",
+      link: "search",
     },
     alternativeMessage: "Alternatively, you can choose a new role below.",
     organisation: "Organisation",
@@ -298,7 +298,7 @@ describe("SelectYourRolePage", () => {
     })
   })
 
-  it("redirects to searchforaprescription when there is one role with access and no roles without access", async () => {
+  it("redirects to search when there is one role with access and no roles without access", async () => {
     const mockNavigate = jest.fn();
     (useNavigate as jest.Mock).mockReturnValue(mockNavigate);
 
