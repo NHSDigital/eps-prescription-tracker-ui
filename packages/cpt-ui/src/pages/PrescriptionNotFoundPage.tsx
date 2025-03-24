f327ed332393300a3d07f4340835a9a8713d4fe2
Jim Wild <james.wild6@nhs.net>
2025-03-24 15:15:48 +0000

    Give the back button a link target

--- packages/cpt-ui/src/pages/PrescriptionNotFoundPage.tsx
+++ packages/cpt-ui/src/pages/PrescriptionNotFoundPage.tsx
@@ -1,15 +1,21 @@
 "use client"
 
+import { Link } from "react-router-dom"
 import { BackLink, Container } from "nhsuk-react-components"
 import { PRESCRIPTION_NOT_FOUND_STRINGS } from "@/constants/ui-strings/PrescriptionNotFoundPageStrings"
 
 export default function PrescriptionNotFoundPage() {
     return (
-        <main>
+        <main id="main-content" className="nhsuk-main-wrapper">
             <Container>
-                <BackLink>{PRESCRIPTION_NOT_FOUND_STRINGS.backLinkText}</BackLink>
-                <h1 data-testid="eps-404-header">{PRESCRIPTION_NOT_FOUND_STRINGS.headerText}</h1>
-                <p data-testid="eps-404-body1">{PRESCRIPTION_NOT_FOUND_STRINGS.body1}</p>
+                <BackLink
+                    asElement={Link}
+                    to="/search"
+                >
+                    {PRESCRIPTION_NOT_FOUND_STRINGS.backLinkText}
+                </BackLink>
+                <h1 data-testid="eps-presc-not-found-header">{PRESCRIPTION_NOT_FOUND_STRINGS.headerText}</h1>
+                <p data-testid="eps-presc-not-found-body1">{PRESCRIPTION_NOT_FOUND_STRINGS.body1}</p>
             </Container>
         </main>
     )
