a3bc66ab523d1b3f0fd85481010c37f06bfe5a0e
Kris Szlapa <kris.szlapa1@nhs.net>
2025-03-25 21:26:42 +0000

    Improve the SearchPrescriptionPage styling

--- packages/cpt-ui/src/pages/SearchPrescriptionPage.tsx
+++ packages/cpt-ui/src/pages/SearchPrescriptionPage.tsx
@@ -1,28 +1,50 @@
-import React from "react";
-import { Col, Container, Hero, Row } from "nhsuk-react-components";
-import EpsTabs from "@/components/EpsTabs";
-import { HERO_TEXT } from "@/constants/ui-strings/SearchForAPrescriptionStrings"
+import React from "react"
+import {Container, Row, Col, Hero} from "nhsuk-react-components"
+import EpsTabs from "@/components/EpsTabs"
+import {HERO_TEXT} from "@/constants/ui-strings/SearchForAPrescriptionStrings"
 
 export default function SearchForAPrescriptionPage() {
   return (
     <>
       <title>Search for a prescription</title>
-      <main id="search-for-a-prescription" data-testid="search-for-a-prescription">
+      <main
+        id="search-for-a-prescription"
+        className="nhsuk-main-wrapper nhsuk-main-wrapper--s"
+        data-testid="search-for-a-prescription"
+        role="main"
+      >
+        {/* Hero Banner */}
         <Container className="hero-container" data-testid="search-hero-container">
           <Row>
             <Col width="full">
-              <Hero className="nhsuk-hero-wrapper" data-testid="hero-banner">
-                <Hero.Heading className="heroHeading" id="hero-heading" data-testid="hero-heading">
+              <Hero className="nhsuk-hero__wrapper">
+                <Hero.Heading
+                  className="nhsuk-u-margin-bottom-3"
+                  id="hero-heading"
+                  data-testid="hero-heading"
+                >
                   {HERO_TEXT}
                 </Hero.Heading>
               </Hero>
             </Col>
           </Row>
         </Container>
-        <Container data-testid="search-tabs-container">
+
+        {/* Tabs */}
+        <div className="nhsuk-tab-set find-patient-tabset" data-testid="search-tabs-container">
+          <div className="find-patient-tabset__container">
+            <EpsTabs />
+          </div>
+        </div>
+
+        {/* Panel Container */}
+        <Container
+          className="nhsuk-width-container-fluid patient-search-form-container"
+          data-testid="search-form-wrapper"
+        >
           <Row>
-            <Col width="full">
-              <EpsTabs />
+            <Col width="one-half">
+              {/* Form content will be rendered by EpsTabs children */}
             </Col>
           </Row>
         </Container>
