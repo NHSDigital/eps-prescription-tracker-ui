40c3117a600c93135fb70f4d817bcfb081db1b8e
Jim Wild <james.wild6@nhs.net>
2025-03-26 14:15:18 +0000

    If the search page is arrived at incorrectly, redirect to the precription not found page

--- packages/cpt-ui/src/pages/PrescriptionListPage.tsx
+++ packages/cpt-ui/src/pages/PrescriptionListPage.tsx
@@ -1,27 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { BackLink, Col, Container, Row } from "nhsuk-react-components";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { PRESCRIPTION_LIST_PAGE_STRINGS } from "@/constants/ui-strings/PrescriptionListPageStrings";
 
 export default function PrescriptionListPage() {
     // TODO: mock data - in the real implementation, this would come from props or context
     const prescriptionCount = 5;
     const location = useLocation();
+    const navigate = useNavigate();
     const [backLinkTarget, setBackLinkTarget] = useState<string>(PRESCRIPTION_LIST_PAGE_STRINGS.DEFAULT_BACK_LINK_TARGET);
+
+    const notFoundRedirect = () => {
+        navigate("/prescription-not-found")
+    }
 
     useEffect(() => {
         // parse the current URL's query parameters
         const queryParams = new URLSearchParams(location.search);
 
+        const hasPrescriptionId = !!queryParams.get("prescriptionId")
+        const hasNhsNumber = !!queryParams.get("nhsNumber")
+
         // determine which search page to go back to based on query parameters
-        if (queryParams.has('prescriptionId')) {
+        if (hasPrescriptionId) {
             setBackLinkTarget(PRESCRIPTION_LIST_PAGE_STRINGS.PRESCRIPTION_ID_SEARCH_TARGET);
-        } else if (queryParams.has('nhsNumber')) {
+        } else if (hasNhsNumber) {
             setBackLinkTarget(PRESCRIPTION_LIST_PAGE_STRINGS.NHS_NUMBER_SEARCH_TARGET);
         } else {
-            // default fallback
-            setBackLinkTarget(PRESCRIPTION_LIST_PAGE_STRINGS.DEFAULT_BACK_LINK_TARGET);
+            // if no search is given, redirect to the not found page
+            notFoundRedirect();
         }
+
+        // If we didn't get any query values, send the user to the not found page
+        const options = [hasPrescriptionId, hasPrescriptionId]
+        if (!options.includes(true)) {
+            notFoundRedirect()
+        }
+
     }, [location]);
 
     return (
