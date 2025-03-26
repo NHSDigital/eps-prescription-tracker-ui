c80495468f2c3a0b3f9c7f1e30a6b47083a887c1
Jim Wild <james.wild6@nhs.net>
2025-03-26 16:18:20 +0000

    Add counts to the tab titles

--- packages/cpt-ui/src/components/prescriptionList/PrescriptionsListTab.tsx
+++ packages/cpt-ui/src/components/prescriptionList/PrescriptionsListTab.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Tabs } from "nhsuk-react-components";
+import React, { useEffect, useState } from 'react'
+import { Tabs } from "nhsuk-react-components"
 
 import PrescriptionsList from "@/components/prescriptionList/PrescriptionsList"
 
@@ -8,8 +8,8 @@ import {
     FUTURE_PRESCRIPTIONS,
     PAST_PRESCRIPTIONS,
     PRESCRIPTION_LIST_TABS
-} from "@/constants/ui-strings/PrescriptionListTabStrings";
-import { PrescriptionSummary } from '@cpt-ui-common/common-types';
+} from "@/constants/ui-strings/PrescriptionListTabStrings"
+import { PrescriptionSummary } from '@cpt-ui-common/common-types'
 
 export interface PrescriptionsListTabsProps {
     currentPrescriptions: PrescriptionSummary[],
@@ -17,47 +17,62 @@ export interface PrescriptionsListTabsProps {
     futurePrescriptions: PrescriptionSummary[]
 }
 
+interface PrescriptionCounts {
+    current: number,
+    past: number,
+    future: number
+}
+
 export default function PrescriptionsListTabs({
     currentPrescriptions,
     pastPrescriptions,
     futurePrescriptions
 }: PrescriptionsListTabsProps) {
-    const tabData = PRESCRIPTION_LIST_TABS;
+    const tabData = PRESCRIPTION_LIST_TABS
+    // Will update when the component re-renders, so no useEffect needed here
+    const prescriptionCounts = {
+        current: currentPrescriptions.length,
+        future: futurePrescriptions.length,
+        past: pastPrescriptions.length
+    }
 
     return (
         <Tabs defaultValue={tabData[0].targetId}>
             <Tabs.Title>Contents</Tabs.Title>
             <Tabs.List>
-                {
-                    tabData.map(tabHeader =>
-                        <Tabs.ListItem id={tabHeader.targetId} key={tabHeader.title}>{tabHeader.title}</Tabs.ListItem>
+                {tabData.map(tabHeader => {
+                    const count = prescriptionCounts[tabHeader.targetId as keyof typeof prescriptionCounts] || 0
+                    return (
+                        <Tabs.ListItem id={tabHeader.targetId} key={tabHeader.title}>
+                            {`${tabHeader.title} (${count})`}
+                        </Tabs.ListItem>
                     )
-                }
+                })}
             </Tabs.List>
-            {
-                tabData.map(tabContent =>
-                    <Tabs.Contents id={tabContent.targetId} key={tabContent.title}>
-                        <div className='search-form-container'>
-                            {
-                                (tabContent.targetId === 'current' && <PrescriptionsList
-                                    textContent={CURRENT_PRESCRIPTIONS}
-                                    prescriptions={currentPrescriptions}
-                                />) ||
-                                (tabContent.targetId === 'future' && <PrescriptionsList
+            {tabData.map(tabContent => (
+                <Tabs.Contents id={tabContent.targetId} key={tabContent.title}>
+                    <div>
+                        {(tabContent.targetId === 'current' && (
+                            <PrescriptionsList
+                                textContent={CURRENT_PRESCRIPTIONS}
+                                prescriptions={currentPrescriptions}
+                            />
+                        )) ||
+                            (tabContent.targetId === 'future' && (
+                                <PrescriptionsList
                                     textContent={FUTURE_PRESCRIPTIONS}
                                     prescriptions={futurePrescriptions}
-                                />) ||
-                                (tabContent.targetId === 'past' && <PrescriptionsList
+                                />
+                            )) ||
+                            (tabContent.targetId === 'past' && (
+                                <PrescriptionsList
                                     textContent={PAST_PRESCRIPTIONS}
                                     prescriptions={pastPrescriptions}
-                                />) ||
-                                <p>This Search not available</p>
-                            }
-                        </div>
-
-                    </Tabs.Contents>
-                )
-            }
+                                />
+                            )) || <p>This Search not available</p>}
+                    </div>
+                </Tabs.Contents>
+            ))}
         </Tabs>
     )
 }
