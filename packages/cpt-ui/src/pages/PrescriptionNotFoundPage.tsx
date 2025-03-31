bab5de0f5005750116953ea1ceacd2b84bcb594d
Bence Gadanyi <bence.gadanyi1@nhs.net>
2025-03-31 15:23:41 +0000

    chore: fixes redirect logic

--- packages/cpt-ui/src/pages/PrescriptionNotFoundPage.tsx
+++ packages/cpt-ui/src/pages/PrescriptionNotFoundPage.tsx
@@ -1,11 +1,29 @@
+import React from "react"
 import {Link, useSearchParams} from "react-router-dom"
 import {BackLink, Container} from "nhsuk-react-components"
 import {PRESCRIPTION_NOT_FOUND_STRINGS} from "@/constants/ui-strings/PrescriptionNotFoundPageStrings"
+import {FRONTEND_PATHS} from "@/constants/environment"
 
 export default function PrescriptionNotFoundPage() {
   const [searchParams] = useSearchParams()
   const searchType = searchParams.get("searchType")
-  const backLinkUrl = `/search${searchType ? `#${searchType}` : ""}`
+
+  // Map searchType to the correct back link URL
+  const getBackLinkUrl = () => {
+    switch (searchType) {
+      case "PrescriptionIdSearch":
+        return FRONTEND_PATHS.SEARCH_BY_PRESCRIPTION_ID
+      case "NhsNumberSearch":
+        return FRONTEND_PATHS.SEARCH_BY_NHS_NUMBER
+      case "BasicDetailsSearch":
+        return FRONTEND_PATHS.SEARCH_BY_BASIC_DETAILS
+      default:
+        // Default fallback if no searchType is provided
+        return FRONTEND_PATHS.SEARCH_BY_PRESCRIPTION_ID
+    }
+  }
+
+  const backLinkUrl = getBackLinkUrl()
 
   return (
     <main id="main-content" className="nhsuk-main-wrapper">
