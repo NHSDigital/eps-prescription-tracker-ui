e2605bb4150f14bbf78777f9f06b3f8aca454b42
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-04-01 14:09:25 +0000

    sync

--- scripts/run_sync.sh
+++ scripts/run_sync.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2034
+set -e
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Script directory: $CURRENT_DIR"
+
+mkdir -p .local_config
+FIX_SCRIPT="${CURRENT_DIR}/../.github/scripts/fix_cdk_json.sh"
+STATEFUL_CONFIG=".local_config/stateful_app.config.json"
+STATELESS_CONFIG=".local_config/stateless_app.config.json"
 
 echo "What is the pull request id?"
 read -r PULL_REQUEST_ID
 
 export SERVICE_NAME=cpt-ui-pr-$PULL_REQUEST_ID
+
+echo "Getting exports from stack ${SERVICE_NAME}"
 
 # variables needed for website
 userPoolClientId=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:userPoolClient:userPoolClientId'].Value" --output text)
@@ -18,10 +27,7 @@ rumAllowCookies=$(aws cloudformation list-exports --region eu-west-2 --query "Ex
 rumEnableXRay=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:rum:config:enableXRay'].Value" --output text)
 rumSessionSampleRate=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:rum:config:sessionSampleRate'].Value" --output text)
 rumTelemetries=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:rum:config:telemetries'].Value" --output text)
-
-# variables needed for StatefulResourcesApp
-CDK_APP_NAME=StatefulResourcesApp
-VERSION_NUMBER=PR-$PULL_REQUEST_ID
+VERSION_NUMBER="PR-${PULL_REQUEST_ID}"
 COMMIT_ID=unknown
 AUTO_DELETE_OBJECTS=true
 unset CLOUDFRONT_DISTRIBUTION_ID
@@ -39,48 +45,17 @@ PRIMARY_OIDC_ISSUER=$(aws cloudformation list-exports --region eu-west-2 --query
 PRIMARY_OIDC_AUTHORIZE_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:primaryOidcAuthorizeEndpoint'].Value" --output text)
 PRIMARY_OIDC_TOKEN_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:primaryOidcUserInfoEndpoint'].Value" --output text)
 PRIMARY_OIDC_USERINFO_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:primaryOidcjwksEndpoint'].Value" --output text)
-PRIMARY_OIDC_JWKS_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:primaryTokenEndpoint'].Value" --output text)
+PRIMARY_OIDC_JWKS_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:primaryOidcTokenEndpoint'].Value" --output text)
 MOCK_OIDC_CLIENT_ID=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockOidcClientId'].Value" --output text)
 MOCK_OIDC_ISSUER=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockOidcIssuer'].Value" --output text)
 MOCK_OIDC_AUTHORIZE_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockOidcAuthorizeEndpoint'].Value" --output text)
-MOCK_OIDC_TOKEN_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockOidcUserInfoEndpoint'].Value" --output text)
-MOCK_OIDC_USERINFO_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockOidcjwksEndpoint'].Value" --output text)
-MOCK_OIDC_JWKS_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockTokenEndpoint'].Value" --output text)
+MOCK_OIDC_TOKEN_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockOidcTokenEndpoint'].Value" --output text)
+MOCK_OIDC_USERINFO_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockOidcUserInfoEndpoint'].Value" --output text)
+MOCK_OIDC_JWKS_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockOidcjwksEndpoint'].Value" --output text)
 LOG_RETENTION_IN_DAYS=30
 LOG_LEVEL=debug
 USE_CUSTOM_COGNITO_DOMAIN=true
 ALLOW_LOCALHOST_ACCESS=true
-
-
-# variables needed for statelessResourcesApp
-CDK_APP_NAME=StatelessResourcesApp
-VERSION_NUMBER=PR-$PULL_REQUEST_ID
-COMMIT_ID=undefined
-AUTO_DELETE_OBJECTS=true
-LOG_RETENTION_IN_DAYS=30
-LOG_LEVEL=debug
-unset EPS_DOMAIN_NAME
-unset EPS_HOSTED_ZONE_ID
-unset CLOUDFRONT_DISTRIBUTION_ID
-unset SHORT_CLOUDFRONT_DOMAIN
-unset FULL_CLOUDFRONT_DOMAIN
-unset FULL_COGNITO_DOMAIN
-unset RUM_LOG_GROUP_ARN
-unset RUM_APP_NAME
-PRIMARY_OIDC_CLIENT_ID=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:primaryOidcClientId'].Value" --output text)
-PRIMARY_OIDC_ISSUER=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:primaryOidcIssuer'].Value" --output text)
-PRIMARY_OIDC_AUTHORIZE_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:primaryOidcAuthorizeEndpoint'].Value" --output text)
-PRIMARY_OIDC_TOKEN_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:primaryOidcUserInfoEndpoint'].Value" --output text)
-PRIMARY_OIDC_USERINFO_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:primaryOidcjwksEndpoint'].Value" --output text)
-PRIMARY_OIDC_JWKS_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:primaryTokenEndpoint'].Value" --output text)
-MOCK_OIDC_CLIENT_ID=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockOidcClientId'].Value" --output text)
-MOCK_OIDC_ISSUER=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockOidcIssuer'].Value" --output text)
-MOCK_OIDC_AUTHORIZE_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockOidcAuthorizeEndpoint'].Value" --output text)
-MOCK_OIDC_TOKEN_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockOidcUserInfoEndpoint'].Value" --output text)
-MOCK_OIDC_USERINFO_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockOidcjwksEndpoint'].Value" --output text)
-MOCK_OIDC_JWKS_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateful-resources:local:mockTokenEndpoint'].Value" --output text)
-USE_MOCK_OIDC=true
-
 APIGEE_API_KEY=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateless-resources:local:apigeeApiKey'].Value" --output text)
 APIGEE_CIS2_TOKEN_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateless-resources:local:apigeeCIS2TokenEndpoint'].Value" --output text)
 APIGEE_MOCK_TOKEN_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateless-resources:local:apigeeMockTokenEndpoint'].Value" --output text)
@@ -88,3 +63,55 @@ APIGEE_PRESCRIPTION_ENDPOINT=$(aws cloudformation list-exports --region eu-west-
 APIGEE_PERSONAL_DEMOGRAPHICS_ENDPOINT=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateless-resources:local:apigeePersonalDemographicsEndpoint'].Value" --output text)
 JWT_KID=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateless-resources:local:jwtKid'].Value" --output text)
 ROLE_ID=$(aws cloudformation list-exports --region eu-west-2 --query "Exports[?Name=='${SERVICE_NAME}-stateless-resources:local:roleId'].Value" --output text)
+
+export VERSION_NUMBER
+export COMMIT_ID
+export AUTO_DELETE_OBJECTS
+export CLOUDFRONT_DISTRIBUTION_ID
+export CLOUDFRONT_CERT_ARN
+export SHORT_CLOUDFRONT_DOMAIN
+export FULL_CLOUDFRONT_DOMAIN
+export EPS_DOMAIN_NAME
+export FULL_COGNITO_DOMAIN
+export RUM_LOG_GROUP_ARN
+export RUM_APP_NAME
+export EPS_HOSTED_ZONE_ID
+export USE_MOCK_OIDC
+export PRIMARY_OIDC_CLIENT_ID
+export PRIMARY_OIDC_ISSUER
+export PRIMARY_OIDC_AUTHORIZE_ENDPOINT
+export PRIMARY_OIDC_TOKEN_ENDPOINT
+export PRIMARY_OIDC_USERINFO_ENDPOINT
+export PRIMARY_OIDC_JWKS_ENDPOINT
+export MOCK_OIDC_CLIENT_ID
+export MOCK_OIDC_ISSUER
+export MOCK_OIDC_AUTHORIZE_ENDPOINT
+export MOCK_OIDC_TOKEN_ENDPOINT
+export MOCK_OIDC_USERINFO_ENDPOINT
+export MOCK_OIDC_JWKS_ENDPOINT
+export LOG_RETENTION_IN_DAYS
+export LOG_LEVEL
+export USE_CUSTOM_COGNITO_DOMAIN
+export ALLOW_LOCALHOST_ACCESS
+export APIGEE_API_KEY
+export APIGEE_CIS2_TOKEN_ENDPOINT
+export APIGEE_MOCK_TOKEN_ENDPOINT
+export APIGEE_PRESCRIPTION_ENDPOINT
+export APIGEE_PERSONAL_DEMOGRAPHICS_ENDPOINT
+export JWT_KID
+export ROLE_ID
+
+# variables needed for StatefulResourcesApp
+CDK_APP_NAME=StatefulResourcesApp
+export CDK_APP_NAME
+
+echo "Runnig ${FIX_SCRIPT} for ${STATEFUL_CONFIG}"
+"$FIX_SCRIPT" "$STATEFUL_CONFIG"
+cat "${STATEFUL_CONFIG}"
+
+
+# variables needed for statelessResourcesApp
+CDK_APP_NAME=StatelessResourcesApp
+export CDK_APP_NAME
+"$FIX_SCRIPT" "$STATELESS_CONFIG"
+cat "${STATELESS_CONFIG}"
