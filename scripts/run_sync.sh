92a27ec4cc913b643943a54094d34c42ca206b34
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-04-01 13:34:26 +0000

    fix it

--- scripts/run_sync.sh
+++ scripts/run_sync.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+echo "Script directory: $CURRENT_DIR"
+
 echo "What is the pull request id?"
 read -r PULL_REQUEST_ID
 
