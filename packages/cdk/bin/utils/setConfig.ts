79479caf6c5ead3e684808b47983389490d3da8f
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-04-01 08:39:02 +0000

    get all keys

--- packages/cdk/bin/utils/setConfig.ts
+++ packages/cdk/bin/utils/setConfig.ts
@@ -1,5 +1,5 @@
 import {Construct} from "constructs"
-import fs from 'fs'
+import fs from "fs"
 
 export class setConfig extends Construct {
   constructor(scope: Construct, id: string) {
@@ -7,7 +7,49 @@ export class setConfig extends Construct {
 
     const configFileName = this.node.tryGetContext("configFileName")
 
-    const configDetails = JSON.parse(fs.readFileSync(configFileName, 'utf-8'))
+    const configDetails = JSON.parse(fs.readFileSync(configFileName, "utf-8"))
 
-    this.node.setContext("primaryOidcClientId", configDetails["primaryOidcClientId"])
+    const contextConfigKeys = [
+      "serviceName",
+      "VERSION_NUMBER",
+      "COMMIT_ID",
+      "logRetentionInDays",
+      "logLevel",
+      "primaryOidcClientId",
+      "primaryOidcIssuer",
+      "primaryOidcAuthorizeEndpoint",
+      "primaryOidcTokenEndpoint",
+      "primaryOidcUserInfoEndpoint",
+      "primaryOidcjwksEndpoint",
+      "useMockOidc",
+      "mockOidcClientId",
+      "mockOidcIssuer",
+      "mockOidcAuthorizeEndpoint",
+      "mockOidcTokenEndpoint",
+      "mockOidcUserInfoEndpoint",
+      "mockOidcjwksEndpoint",
+      "useCustomCognitoDomain",
+      "epsDomainName",
+      "epsHostedZoneId",
+      "allowAutoDeleteObjects",
+      "cloudfrontDistributionId",
+      "rumCloudwatchLogEnabled",
+      "rumAppName",
+      "allowLocalhostAccess",
+      "cloudfrontCertArn",
+      "shortCloudfrontDomain",
+      "fullCloudfrontDomain",
+      "fullCognitoDomain",
+      "apigeeMockTokenEndpoint",
+      "apigeeApiKey",
+      "apigeeCIS2TokenEndpoint",
+      "apigeePrescriptionsEndpoint",
+      "apigeePersonalDemographicsEndpoint",
+      "jwtKid",
+      "ROLE_ID"
+    ]
+    contextConfigKeys.forEach((key) => {
+      this.node.setContext(key, configDetails[key])
+    })
+  }
 }
