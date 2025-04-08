08229551fe654835e1cc1d3a6fa9b5aab8b72b19
Kris Szlapa <kris.szlapa1@nhs.net>
2025-04-08 11:47:00 +0000

    Disable max-len rule instead of tabbing the code

--- packages/cpt-ui/global.d.ts
+++ packages/cpt-ui/global.d.ts
@@ -2,9 +2,7 @@ export {}
 
 declare global {
   interface Window {
-    __mockedPatientDetails?: import("@/context/PatientDetailsProvider")
-    .PatientDetailsContextType["patientDetails"]
-    __mockedPrescriptionInformation?: import("@/context/PrescriptionInformationProvider")
-    .PrescriptionInformation | undefined
+    __mockedPatientDetails?: import("@/context/PatientDetailsProvider").PatientDetailsContextType["patientDetails"]
+    __mockedPrescriptionInformation?: import("@/context/PrescriptionInformationProvider").PrescriptionInformation | undefined // eslint-disable-line max-len
   }
 }
