e742c666704328aeb69d42e41afefb56f7ca9452
Jim Wild <james.wild6@nhs.net>
2025-04-11 13:38:46 +0000

    Update hidden header wording

--- packages/cpt-ui/src/constants/ui-strings/PrescriptionDetailsPageStrings.ts
+++ packages/cpt-ui/src/constants/ui-strings/PrescriptionDetailsPageStrings.ts
@@ -1,4 +1,4 @@
 export const HEADER = "Prescription details"
 export const GO_BACK = "Go back"
-export const PAGE_TITLE = "Prescription details"
+export const PAGE_TITLE = "Full prescription details"
 export const LOADING_FULL_PRESCRIPTION = "Loading full prescription"
