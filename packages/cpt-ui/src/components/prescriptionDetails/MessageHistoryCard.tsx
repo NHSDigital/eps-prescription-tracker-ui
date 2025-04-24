2bfa4d6443601b132ed4c6ec621325e9263597c4
Kris Szlapa <kris.szlapa1@nhs.net>
2025-04-24 17:14:58 +0000

    Add the logic to loop through the history messages

--- packages/cpt-ui/src/components/prescriptionDetails/MessageHistoryCard.tsx
+++ packages/cpt-ui/src/components/prescriptionDetails/MessageHistoryCard.tsx
@@ -10,28 +10,30 @@ interface MessageHistoryProps {
 export function MessageHistoryCard({messageHistory}: MessageHistoryProps) {
   if (!messageHistory || messageHistory.length === 0) return null
 
-  const firstMessage = messageHistory[0]
-
   return (
     <Col width="one-third">
       <h2 className="nhsuk-heading-xs nhsuk-u-margin-bottom-2">{STRINGS.HISTORY_HEADER}</h2>
       <div className="data-panel__wrapper no-outline" tabIndex={-1}>
         <Card className="nhsuk-u-margin-bottom-3 data-panel" style={{boxShadow: "none"}}>
           <Card.Content className="nhsuk-u-padding-4">
-            <Card.Heading headingLevel="H3" className="nhsuk-heading-xs nhsuk-u-margin-bottom-1">
-              {getMessageHistoryHeader(firstMessage.messageText)}
-              <br />
-              {firstMessage.sentDateTime}
-            </Card.Heading>
-            <p className="nhsuk-body-s nhsuk-u-margin-bottom-2">
-              {STRINGS.ORGANISATION} {firstMessage.organisationName} ({STRINGS.ODS_TEXT}{firstMessage.organisationODS})
-            </p>
-            <p className="nhsuk-body-s nhsuk-u-margin-bottom-2">
-              {STRINGS.NEW_STATUS} {" "}
-              <Tag color={getStatusTagColour(firstMessage.newStatusCode)}>
-                {getStatusDisplayText(firstMessage.newStatusCode)}
-              </Tag>
-            </p>
+            {messageHistory.map((msg, index) => (
+              <div key={index} className="nhsuk-u-margin-bottom-4">
+                <Card.Heading headingLevel="H3" className="nhsuk-heading-xs nhsuk-u-margin-bottom-1">
+                  {getMessageHistoryHeader(msg.messageText)}
+                  <br />
+                  {msg.sentDateTime}
+                </Card.Heading>
+                <p className="nhsuk-body-s nhsuk-u-margin-bottom-2">
+                  {STRINGS.ORGANISATION} {msg.organisationName} ({STRINGS.ODS_TEXT}{msg.organisationODS})
+                </p>
+                <p className="nhsuk-body-s nhsuk-u-margin-bottom-2">
+                  {STRINGS.NEW_STATUS}{" "}
+                  <Tag color={getStatusTagColour(msg.newStatusCode)}>
+                    {getStatusDisplayText(msg.newStatusCode)}
+                  </Tag>
+                </p>
+              </div>
+            ))}
           </Card.Content>
         </Card>
       </div>
