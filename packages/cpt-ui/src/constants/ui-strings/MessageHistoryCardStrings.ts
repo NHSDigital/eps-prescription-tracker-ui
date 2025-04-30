267b6de04bad5b6dd941c8970ccf5ec5ba7ad45c
Kris Szlapa <kris.szlapa1@nhs.net>
2025-04-30 09:16:13 +0100

    New: [AEA-4801] - Message History (#756)
    ## Summary
    
    🎫 [AEA-4801](https://nhsd-jira.digital.nhs.uk/browse/AEA-4801) Message
    History
    🧪 Regression Tests:
    https://github.com/NHSDigital/electronic-prescription-service-api-regression-tests/pull/299
    
    :sparkles: New Feature
    
    ### Details
    
    This is a timeline part of the Prescription Details which give the user
    further background of the history of the Prescription.

---
+++ packages/cpt-ui/src/constants/ui-strings/MessageHistoryCardStrings.ts
@@ -0,0 +1,12 @@
+export const STRINGS = {
+  HISTORY_HEADER: "History",
+  ORGANISATION: "Organisation:",
+  NEW_STATUS: "New status:",
+  ODS_TEXT: "ODS:",
+  DISPENSE_NOTIFICATION_INFO: "Dispense notification information",
+  DISPENSE_NOTIFICATION_ID: "Dispense notification ID:",
+  PRESCRIPTION_ITEMS: "Prescription items:",
+  QUANTITY: "Quantity",
+  INSTRUCTIONS: "Instructions",
+  NO_ORG_NAME: "Organisation name not available. Try again later."
+}
