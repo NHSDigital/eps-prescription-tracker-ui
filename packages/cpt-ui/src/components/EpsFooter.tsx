be911e005ebfec60ae98e80e36561126d6da23fd
Kris Szlapa <kris.szlapa1@nhs.net>
2025-05-02 08:04:20 +0000

    Add links to the footer

--- packages/cpt-ui/src/components/EpsFooter.tsx
+++ packages/cpt-ui/src/components/EpsFooter.tsx
@@ -1,30 +1,36 @@
 import React, {useEffect} from "react"
-
 import {Footer} from "nhsuk-react-components"
 
-import {FOOTER_COPYRIGHT, COMMIT_ID} from "@/constants/ui-strings/FooterStrings"
+import {FOOTER_COPYRIGHT, COMMIT_ID, FOOTER_LINKS} from "@/constants/ui-strings/FooterStrings"
 
 export default function EpsFooter() {
-
   useEffect(() => {
     console.log("Viewing site version of commit ID:", COMMIT_ID)
   }, [])
 
   return (
     <Footer id="eps_footer" className="eps_footer">
-      {COMMIT_ID ? (
-        <Footer.List>
-          <small>
-            {COMMIT_ID}
-          </small>
-        </Footer.List>
-      ) : (
-        <div />
-      )}
+      <Footer.List>
+        {FOOTER_LINKS.map(({href, text, external}, index) => (
+          <Footer.ListItem key={index}>
+            <a
+              className="nhsuk-footer__list-item-link"
+              href={href}
+              target={external ? "_blank" : undefined}
+              rel={external ? "noopener noreferrer" : undefined}
+            >
+              {text}
+            </a>
+          </Footer.ListItem>
+        ))}
+      </Footer.List>
+
       <Footer.Copyright data-testid="eps_footer-copyright">
-        <small>
-          {FOOTER_COPYRIGHT}
-        </small>
+        <p>
+          <small>
+            {FOOTER_COPYRIGHT}
+          </small>
+        </p>
       </Footer.Copyright>
     </Footer>
   )
--- packages/cpt-ui/src/constants/ui-strings/FooterStrings.ts
+++ packages/cpt-ui/src/constants/ui-strings/FooterStrings.ts
@@ -1,2 +1,21 @@
 export const FOOTER_COPYRIGHT = "© NHS England"
 export const COMMIT_ID = import.meta.env.VITE_COMMIT_ID
+
+export const FOOTER_LINKS = [
+  {
+    text: "Privacy notice",
+    // eslint-disable-next-line max-len
+    href: "https://digital.nhs.uk/services/care-identity-service/registration-authority-users/registration-authority-help/privacy-notice",
+    external: true
+  },
+  {
+    text: "Terms and conditions (opens in new tab)",
+    href: "https://www.nhs.uk/terms-and-conditions",
+    external: false
+  },
+  {
+    text: "Cookie policy",
+    href: "cookies",
+    external: false
+  }
+]
