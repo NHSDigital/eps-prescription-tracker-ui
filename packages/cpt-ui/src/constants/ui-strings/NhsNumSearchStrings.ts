431a586b45b031a86b72018838724954ffdd07cf
Kris Szlapa <kris.szlapa1@nhs.net>
2025-05-02 15:41:42 +0100

    New: [AEA-4784] - Search by NHS Number - CPTS UI (#781)
    ## Summary
    
    🎫 [AEA-4784](https://nhsd-jira.digital.nhs.uk/browse/AEA-4784) Search by
    NHS Number - CPTS UI
    🧪 Regression Tests:
    https://github.com/NHSDigital/electronic-prescription-service-api-regression-tests/pull/304
    
    :sparkles: New Feature
    
    ### Details
    
    This feature introduces the ability to search for a patient within the
    CPTS by their NHS number.
    In certain care settings, users managing or providing patient care often
    have direct access to a patient's NHS number. By enabling NHS number
    search functionality, users can now locate prescriptions faster and more
    efficiently within the CPTS.

---
+++ packages/cpt-ui/src/constants/ui-strings/NhsNumSearchStrings.ts
@@ -0,0 +1,12 @@
+export const STRINGS = {
+  labelText: "Search using NHS number",
+  hintText: "This is a 10 digit number, like 485 777 3456.",
+  hiddenText: "Search by",
+  buttonText: "Find a patient",
+  errorSummaryHeading: "There is a problem",
+  errors: {
+    empty: "Enter an NHS number",
+    length: "NHS number must have 10 digits",
+    chars: "Enter an NHS number in the correct format"
+  }
+}
