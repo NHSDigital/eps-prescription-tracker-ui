83465a90b8cf3690ce17246f2148c699e13023fc
Kris Szlapa <kris.szlapa1@nhs.net>
2025-05-06 14:27:53 +0000

    Update the terms and conditions link

--- packages/cpt-ui/src/constants/ui-strings/FooterStrings.ts
+++ packages/cpt-ui/src/constants/ui-strings/FooterStrings.ts
@@ -3,16 +3,16 @@ export const COMMIT_ID = import.meta.env.VITE_COMMIT_ID
 
 export const FOOTER_LINKS = [
   {
-    text: "Privacy notice (opens in new tab)",
-    // eslint-disable-next-line max-len
-    href: "https://digital.nhs.uk/services/care-identity-service/registration-authority-users/registration-authority-help/privacy-notice",
-    external: true,
+    text: "Privacy notice",
+    href: "/site/privacy-notice",
+    external: false,
     testId: "eps_footer-link-privacy-notice"
   },
   {
-    text: "Terms and conditions",
-    href: "/site/terms-and-conditions",
-    external: false,
+    text: "Terms and conditions (opens in new tab)",
+    // eslint-disable-next-line max-len
+    href: "https://digital.nhs.uk/services/care-identity-service/registration-authority-users/registration-authority-help/privacy-notice",
+    external: true,
     testId: "eps_footer-link-terms-and-conditions"
   },
   {
