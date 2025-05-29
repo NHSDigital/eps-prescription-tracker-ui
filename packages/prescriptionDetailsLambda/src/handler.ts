c57b6c33d08ca4135078e9680ccf383a028a84fe
Bence Gadanyi <bence.gadanyi1@nhs.net>
2025-05-29 16:00:26 +0000

    refactor: factory pattern based handler

--- packages/prescriptionDetailsLambda/src/handler.ts
+++ packages/prescriptionDetailsLambda/src/handler.ts
@@ -4,88 +4,90 @@ import {getSecret} from "@aws-lambda-powertools/parameters/secrets"
 import {Logger} from "@aws-lambda-powertools/logger"
 import {injectLambdaContext} from "@aws-lambda-powertools/logger/middleware"
 
-import axios from "axios"
-
 import middy from "@middy/core"
 import inputOutputLogger from "@middy/input-output-logger"
+import httpHeaderNormalizer from "@middy/http-header-normalizer"
 
 import {DynamoDBClient} from "@aws-sdk/client-dynamodb"
 import {DynamoDBDocumentClient} from "@aws-sdk/lib-dynamodb"
+import {v4 as uuidv4} from "uuid"
 
-import {
-  getUsernameFromEvent,
-  fetchAndVerifyCIS2Tokens,
-  constructSignedJWTBody,
-  exchangeTokenForApigeeAccessToken,
-  updateApigeeAccessToken,
-  initializeOidcConfig
-} from "@cpt-ui-common/authFunctions"
+import {getUsernameFromEvent, authenticateRequest} from "@cpt-ui-common/authFunctions"
 import {MiddyErrorHandler} from "@cpt-ui-common/middyErrorHandler"
 
-import {processPrescriptionRequest} from "./utils/prescriptionService"
+import {processPrescriptionRequest} from "./services/prescriptionService"
 
-// Logger initialization
-const logger = new Logger({serviceName: "prescriptionDetails"})
+type HandlerParameters = {
+  logger: Logger,
+  documentClient: DynamoDBDocumentClient,
+  apigeePrescriptionsEndpoint: string,
+  tokenMappingTableName: string,
+  jwtPrivateKeyArn: string,
+  apigeeApiKey: string,
+  apigeeApiSecret: string,
+  jwtKid: string,
+  apigeeMockTokenEndpoint: string,
+  apigeeCis2TokenEndpoint: string
+}
 
-// External endpoints and environment variables
-const apigeeCIS2TokenEndpoint = process.env["apigeeCIS2TokenEndpoint"] as string
-const apigeeMockTokenEndpoint = process.env["apigeeMockTokenEndpoint"] as string
+export type HandlerInitialisationParameters = {
+  errorResponseBody: object,
+  logger: Logger,
+  documentClient: DynamoDBDocumentClient,
+  apigeePrescriptionsEndpoint: string,
+  tokenMappingTableName: string,
+  jwtPrivateKeyArn: string,
+  apigeeApiKey: string,
+  apigeeApiSecret: string,
+  jwtKid: string,
+  apigeeMockTokenEndpoint: string,
+  apigeeCis2TokenEndpoint: string
+}
 
-// TODO: Replace hardcoded value with environment variable before production
-// The Apigee API base endpoint for prescription tracking
-// const apigeePrescriptionsEndpoint = process.env["apigeePrescriptionsEndpoint"] as string
-const apigeePrescriptionsEndpoint = "https://internal-dev.api.service.nhs.uk/clinical-prescription-tracker-pr-809/"
-
-const TokenMappingTableName = process.env["TokenMappingTableName"] as string
-
-const jwtPrivateKeyArn = process.env["jwtPrivateKeyArn"] as string
-const jwtKid = process.env["jwtKid"] as string
-
-const apigeeApiKey = process.env["apigeeApiKey"] as string
-const roleId = process.env["roleId"] as string
-
-// DynamoDB client setup
-const dynamoClient = new DynamoDBClient()
-const documentClient = DynamoDBDocumentClient.from(dynamoClient)
-
-// Create a config for cis2 and mock
-// this is outside functions so it can be re-used and caching works
-const {mockOidcConfig, cis2OidcConfig} = initializeOidcConfig()
-
-// Error response template
-const errorResponseBody = {message: "A system error has occurred"}
-
-// Middleware error handler
-const middyErrorHandler = new MiddyErrorHandler(errorResponseBody)
-
-const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const lambdaHandler = async (
+  event: APIGatewayProxyEvent,
+  {
+    logger,
+    documentClient,
+    apigeePrescriptionsEndpoint,
+    tokenMappingTableName,
+    jwtPrivateKeyArn,
+    apigeeApiKey,
+    apigeeApiSecret,
+    jwtKid,
+    apigeeMockTokenEndpoint,
+    apigeeCis2TokenEndpoint
+  }: HandlerParameters
+): Promise<APIGatewayProxyResult> => {
   // Attach request ID for tracing
   logger.appendKeys({"apigw-request-id": event.requestContext?.requestId})
+
+  // Handle x-request-id and x-correlation-id headers
+  const headers = event.headers ?? []
+  logger.appendKeys({"x-request-id": headers["x-request-id"]})
+
+  const correlationId = headers["x-correlation-id"] || uuidv4()
+
   logger.info("Lambda handler invoked", {event})
 
-  const axiosInstance = axios.create()
-
-  const MOCK_MODE_ENABLED = process.env["MOCK_MODE_ENABLED"]
-
-  // ****************************************
-  // SETUP
-  // ****************************************
-
-  // Mock usernames start with "Mock_", and real requests use usernames starting with "Primary_"
+  // Use the authenticateRequest function for authentication
   const username = getUsernameFromEvent(event)
-  const isMockToken = username.startsWith("Mock_")
 
-  // Determine whether this request should be treated as mock or real.
-  if (isMockToken && MOCK_MODE_ENABLED !== "true") {
-    throw new Error("Trying to use a mock user when mock mode is disabled")
-  }
-  const isMockRequest = MOCK_MODE_ENABLED === "true" && isMockToken
+  const {apigeeAccessToken, roleId, orgCode} = await authenticateRequest(username, documentClient, logger, {
+    tokenMappingTableName,
+    jwtPrivateKeyArn,
+    apigeeApiKey,
+    apigeeApiSecret,
+    jwtKid,
+    apigeeMockTokenEndpoint,
+    apigeeCis2TokenEndpoint
+  })
 
-  const apigeeTokenEndpoint = isMockRequest
-    ? apigeeMockTokenEndpoint
-    : apigeeCIS2TokenEndpoint
-
-  logger.info("Is this a mock request?", {isMockRequest})
+  // Log the token for debugging (redacted for security)
+  logger.debug("Using Apigee access token and role", {
+    apigeeAccessToken: apigeeAccessToken,
+    roleId: roleId
+  })
 
   // Fetch the private key for signing the client assertion
   const jwtPrivateKey = await getSecret(jwtPrivateKeyArn)
@@ -93,48 +95,6 @@ const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPro
     throw new Error("Invalid or missing JWT private key")
   }
   logger.info("JWT private key retrieved successfully")
-
-  // ****************************************
-  // TOKEN EXCHANGE & AUTH FLOW
-  // ****************************************
-
-  // Fetch CIS2 tokens
-  const {cis2AccessToken, cis2IdToken} = await fetchAndVerifyCIS2Tokens(
-    event,
-    documentClient,
-    logger,
-    isMockRequest ? mockOidcConfig : cis2OidcConfig
-  )
-  logger.info("Successfully fetched CIS2 tokens", {cis2AccessToken, cis2IdToken})
-
-  // Construct a new body with the signed JWT client assertion
-  const requestBody = constructSignedJWTBody(
-    logger,
-    apigeeTokenEndpoint,
-    jwtPrivateKey,
-    apigeeApiKey,
-    jwtKid,
-    cis2IdToken
-  )
-  logger.info("Constructed request body for Apigee token exchange", {requestBody})
-
-  // Exchange token with Apigee
-  const {accessToken: apigeeAccessToken, expiresIn} = await exchangeTokenForApigeeAccessToken(
-    axiosInstance,
-    apigeeTokenEndpoint,
-    requestBody,
-    logger
-  )
-
-  // Update DynamoDB with the new Apigee access token
-  await updateApigeeAccessToken(
-    documentClient,
-    TokenMappingTableName,
-    event.requestContext.authorizer?.claims?.["cognito:username"] || "unknown",
-    apigeeAccessToken,
-    expiresIn,
-    logger
-  )
 
   // ****************************************
   // PROCESS REQUEST
@@ -146,12 +106,74 @@ const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPro
     apigeePrescriptionsEndpoint,
     apigeeAccessToken,
     roleId,
+    orgCode,
+    correlationId,
     logger
   )
 }
 
-// Export the Lambda function with middleware applied
-export const handler = middy(lambdaHandler)
-  .use(injectLambdaContext(logger, {clearState: true}))
-  .use(inputOutputLogger({logger: (request) => logger.info(request)}))
-  .use(middyErrorHandler.errorHandler({logger}))
+export const newHandler = (
+  initParams: HandlerInitialisationParameters
+) => {
+  const params: HandlerParameters = {
+    logger: initParams.logger,
+    documentClient: initParams.documentClient,
+    apigeePrescriptionsEndpoint: initParams.apigeePrescriptionsEndpoint,
+    tokenMappingTableName: initParams.tokenMappingTableName,
+    jwtPrivateKeyArn: initParams.jwtPrivateKeyArn,
+    apigeeApiKey: initParams.apigeeApiKey,
+    apigeeApiSecret: initParams.apigeeApiSecret,
+    jwtKid: initParams.jwtKid,
+    apigeeMockTokenEndpoint: initParams.apigeeMockTokenEndpoint,
+    apigeeCis2TokenEndpoint: initParams.apigeeCis2TokenEndpoint
+  }
+
+  return middy((event: APIGatewayProxyEvent) => lambdaHandler(event, params))
+    .use(injectLambdaContext(initParams.logger, {clearState: true}))
+    .use(httpHeaderNormalizer())
+    .use(
+      inputOutputLogger({
+        logger: (request) => {
+          initParams.logger.info(request)
+        }
+      })
+    )
+    .use(
+      new MiddyErrorHandler(initParams.errorResponseBody)
+        .errorHandler({logger: initParams.logger})
+    )
+}
+
+// External endpoints and environment variables
+const apigeeCis2TokenEndpoint = process.env["apigeeCIS2TokenEndpoint"] as string
+const apigeeMockTokenEndpoint = process.env["apigeeMockTokenEndpoint"] as string
+// const apigeePrescriptionsEndpoint = process.env["apigeePrescriptionsEndpoint"] as string
+const apigeePrescriptionsEndpoint = "https://internal-dev.api.service.nhs.uk/clinical-prescription-tracker-pr-809/"
+const TokenMappingTableName = process.env["TokenMappingTableName"] as string
+const jwtPrivateKeyArn = process.env["jwtPrivateKeyArn"] as string
+const jwtKid = process.env["jwtKid"] as string
+const apigeeApiKey = process.env["apigeeApiKey"] as string
+const apigeeApiSecret= process.env["APIGEE_API_SECRET"] as string
+
+// DynamoDB client setup
+const dynamoClient = new DynamoDBClient()
+const documentClient = DynamoDBDocumentClient.from(dynamoClient)
+
+// Error response template
+const errorResponseBody = {message: "A system error has occurred"}
+
+const DEFAULT_HANDLER_PARAMETERS: HandlerInitialisationParameters = {
+  errorResponseBody,
+  logger: new Logger({serviceName: "prescriptionDetails"}),
+  documentClient,
+  apigeePrescriptionsEndpoint,
+  tokenMappingTableName: TokenMappingTableName,
+  jwtPrivateKeyArn,
+  apigeeApiKey,
+  apigeeApiSecret,
+  jwtKid,
+  apigeeMockTokenEndpoint,
+  apigeeCis2TokenEndpoint
+}
+
+export const handler = newHandler(DEFAULT_HANDLER_PARAMETERS)
