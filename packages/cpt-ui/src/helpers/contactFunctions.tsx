06371a73e04365e977c68083532a19f4e596dd07
Jonno <jonathan.welch1@nhs.net>
2025-06-11 10:15:11 +0000

    commit

--- packages/cpt-ui/src/helpers/contactFunctions.tsx
+++ packages/cpt-ui/src/helpers/contactFunctions.tsx
@@ -2,7 +2,6 @@ export const createEmailLink = (email: string, text: string | null = null) => (
   <a href={`mailto:${email}`}>{text || email}</a>
 )
 
-// Helper function to create tel links
 export const createPhoneLink = (phone: string, text: string | null = null) => (
   <a href={`tel:${phone.replace(/\s/g, "")}`}>{text || phone}</a>
 )
