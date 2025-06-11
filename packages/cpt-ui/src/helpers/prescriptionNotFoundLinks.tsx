30662410efeffbd029e9dadbce87dd7ad2bf1337
Kris Szlapa <kris.szlapa1@nhs.net>
2025-06-11 08:04:06 +0000

    Update helper function to build alternative search links

--- packages/cpt-ui/src/helpers/prescriptionNotFoundLinks.tsx
+++ packages/cpt-ui/src/helpers/prescriptionNotFoundLinks.tsx
@@ -43,33 +43,15 @@ export const searchTypeToParams: Record<AllowedSearchType, Array<string>> = {
 
 type AltType = { to: string; label: string }
 
-// Helper to build the params for links
+// Helper to build alternative search links
 export function buildAltLink({
-  alt,
-  searchParams
+  alt
 }: {
   alt: AltType
-  searchParams: URLSearchParams
 }) {
-  // Infer AllowedSearchType from alt.to
-  let altType: AllowedSearchType
-  if (alt.to === "SEARCH_BY_PRESCRIPTION_ID") {
-    altType = SEARCH_TYPES.PRESCRIPTION_ID
-  } else if (alt.to === "SEARCH_BY_NHS_NUMBER") {
-    altType = SEARCH_TYPES.NHS_NUMBER
-  } else {
-    altType = SEARCH_TYPES.BASIC_DETAILS
-  }
-
-  const altParamsToKeep = searchTypeToParams[altType]
-  const altParams = new URLSearchParams()
-  altParamsToKeep.forEach((key) => {
-    const val = searchParams.get(key)
-    if (val) altParams.set(key, val)
-  })
   const altPath = FRONTEND_PATHS[alt.to as keyof typeof FRONTEND_PATHS]
   return (
-    <Link key={alt.to} to={`${altPath}?${altParams.toString()}`}>
+    <Link key={alt.to} to={altPath}>
       {alt.label}
     </Link>
   )
