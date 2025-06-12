15cd2c3172814468616b5267a55f52905cf32bb0
Anthony Brown <121869075+anthony-nhs@users.noreply.github.com>
2025-06-12 13:40:07 +0000

    use different domain for int and prod

--- .github/scripts/fix_cdk_json.sh
+++ .github/scripts/fix_cdk_json.sh
@@ -50,10 +50,18 @@ if [ "${DO_NOT_GET_AWS_EXPORT}" != "true" ]; then
 fi
 
 if [ -z "${EPS_DOMAIN_NAME}" ]; then
-    EPS_DOMAIN_NAME=$(echo "$CF_LONDON_EXPORTS" | jq  -r '.Exports[] | select(.Name == "eps-route53-resources:EPS-domain") | .Value')
+    if [[ "$TARGET_ENVIRONMENT" == "int" || "$TARGET_ENVIRONMENT" == "prod" ]]; then
+        EPS_DOMAIN_NAME=$(echo "$CF_LONDON_EXPORTS" | jq  -r '.Exports[] | select(.Name == "eps-route53-resources:CPT-domain") | .Value')
+    else
+        EPS_DOMAIN_NAME=$(echo "$CF_LONDON_EXPORTS" | jq  -r '.Exports[] | select(.Name == "eps-route53-resources:EPS-domain") | .Value')
+    fi
 fi
 if [ -z "${EPS_HOSTED_ZONE_ID}" ]; then
-    EPS_HOSTED_ZONE_ID=$(echo "$CF_LONDON_EXPORTS" | jq -r '.Exports[] | select(.Name == "eps-route53-resources:EPS-ZoneID") | .Value')
+    if [[ "$TARGET_ENVIRONMENT" == "int" || "$TARGET_ENVIRONMENT" == "prod" ]]; then
+        EPS_HOSTED_ZONE_ID=$(echo "$CF_LONDON_EXPORTS" | jq -r '.Exports[] | select(.Name == "eps-route53-resources:CPT-ZoneID") | .Value')
+    else
+        EPS_HOSTED_ZONE_ID=$(echo "$CF_LONDON_EXPORTS" | jq -r '.Exports[] | select(.Name == "eps-route53-resources:EPS-ZoneID") | .Value')
+    fi
 fi
 if [ -z "${CLOUDFRONT_DISTRIBUTION_ID}" ]; then
     CLOUDFRONT_DISTRIBUTION_ID=$(echo "$CF_LONDON_EXPORTS" | \
