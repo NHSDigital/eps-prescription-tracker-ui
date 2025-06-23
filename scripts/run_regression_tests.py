af4ebee622eed5fd359d4d27a00bd20bd9fa0c80
Jonno <jonathan.welch1@nhs.net>
2025-06-23 14:28:34 +0000

    updated repo tag

--- scripts/run_regression_tests.py
+++ scripts/run_regression_tests.py
@@ -13,7 +13,7 @@ import time
 from requests.auth import HTTPBasicAuth
 
 # This should be set to a known good version of regression test repo
-REGRESSION_TESTS_REPO_TAG = "aea-5480-dont-change-tabs-when-typing"
+REGRESSION_TESTS_REPO_TAG = "v2.39.7"
 
 GITHUB_API_URL = "https://api.github.com/repos/NHSDigital/electronic-prescription-service-api-regression-tests/actions"
 GITHUB_RUN_URL = "https://github.com/NHSDigital/electronic-prescription-service-api-regression-tests/actions/runs"
