1161fc4f2e3e99b47b9f0ed53d0b9c5eb3001156
Jonno <jonathan.welch1@nhs.net>
2025-06-23 09:55:45 +0000

    moved incorrect data-test-id and fixed label being in incorrect place

--- packages/cpt-ui/src/components/prescriptionSearch/PrescriptionIdSearch.tsx
+++ packages/cpt-ui/src/components/prescriptionSearch/PrescriptionIdSearch.tsx
@@ -102,18 +102,21 @@ export default function PrescriptionIdSearch() {
               </ErrorSummary>
             )}
 
-            <FormGroup data-testId="search-by-prescriptionid-box" className={errorKey ? "nhsuk-form-group--error" : ""}>
+            <FormGroup className={errorKey ? "nhsuk-form-group--error" : ""}>
+              <h2
+                className="nhsuk-heading-m nhsuk-u-margin-bottom-1 no-outline"
+                data-testid="prescription-id-search-heading"
+              >
+                {PRESCRIPTION_ID_SEARCH_STRINGS.labelText}
+              </h2>
+
               <Label htmlFor="presc-id-input" id="presc-id-label">
-                <h2
-                  className="nhsuk-heading-m nhsuk-u-margin-bottom-1 no-outline"
-                  data-testid="prescription-id-search-heading"
-                >
-                  {PRESCRIPTION_ID_SEARCH_STRINGS.labelText}
-                </h2>
-                <HintText id="presc-id-hint" data-testid="prescription-id-hint">
-                  {PRESCRIPTION_ID_SEARCH_STRINGS.hintText}
-                </HintText>
+                {PRESCRIPTION_ID_SEARCH_STRINGS.labelText}
               </Label>
+
+              <HintText id="presc-id-hint" data-testid="prescription-id-hint">
+                {PRESCRIPTION_ID_SEARCH_STRINGS.hintText}
+              </HintText>
 
               <ErrorMessage id="presc-id-error" data-testid="prescription-id-error">
                 {errorKey ? displayedError : ""}
@@ -126,8 +129,8 @@ export default function PrescriptionIdSearch() {
                 onChange={handleInputChange}
                 className={errorKey ? "nhsuk-input nhsuk-input--error" : "nhsuk-input"}
                 autoComplete="off"
-                data-testid="prescription-id-input"
                 aria-describedby={errorKey ? "presc-id-error" : undefined}
+                data-testid="search-by-prescriptionid-box"
               />
             </FormGroup>
 
