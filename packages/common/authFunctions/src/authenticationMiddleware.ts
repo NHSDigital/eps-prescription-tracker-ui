4736bc8a80a4ec16143a2dac1739e5553813a962
Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>
2025-07-30 16:10:09 +0000

    Restore auth middleware check for existing session
    Signed-off-by: Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>

--- packages/common/authFunctions/src/authenticationMiddleware.ts
+++ packages/common/authFunctions/src/authenticationMiddleware.ts
@@ -5,7 +5,7 @@ import {authenticateRequest, AuthenticateRequestOptions, AuthResult} from "./aut
 import {DynamoDBDocumentClient} from "@aws-sdk/lib-dynamodb"
 import {Logger} from "@aws-lambda-powertools/logger"
 import {AxiosInstance} from "axios"
-import {TokenMappingItem, checkTokenMappingForUser, getTokenMapping} from "@cpt-ui-common/dynamoFunctions"
+import {TokenMappingItem, checkTokenMappingForUser} from "@cpt-ui-common/dynamoFunctions"
 
 export const authenticationMiddleware = (
   axiosInstance: AxiosInstance,
@@ -18,7 +18,7 @@ export const authenticationMiddleware = (
     const username = getUsernameFromEvent(event)
     const sessionId = getSessionIdFromEvent(event)
 
-    let tokenMappingItem: TokenMappingItem
+    // let tokenMappingItem: TokenMappingItem
     let sessionManagementItem: TokenMappingItem | undefined = undefined
 
     // Capture mapping items from token mapping and session management
@@ -37,18 +37,18 @@ export const authenticationMiddleware = (
         will be unable to determine which session instantiated the call.")
     }
 
-    try {
-      tokenMappingItem = await getTokenMapping(
-        ddbClient,
-        authOptions.tokenMappingTableName,
-        username,
-        logger
-      )
-    } catch (error) {
-      logger.error("Failed to query token mapping table.", {error})
-      throw new Error("Failed to query token mapping table, \
-        will be unable to determine which session instantiated the call.")
-    }
+    // try {
+    //   tokenMappingItem = await getTokenMapping(
+    //     ddbClient,
+    //     authOptions.tokenMappingTableName,
+    //     username,
+    //     logger
+    //   )
+    // } catch (error) {
+    //   logger.error("Failed to query token mapping table.", {error})
+    //   throw new Error("Failed to query token mapping table, \
+    //     will be unable to determine which session instantiated the call.")
+    // }
 
     let authResult: AuthResult | null = null
 
@@ -57,7 +57,7 @@ export const authenticationMiddleware = (
       if (sessionManagementItem !== null && sessionManagementItem?.sessionId === sessionId) {
         authResult = await authenticateRequest(username, axiosInstance, ddbClient, logger,
           authOptions, authOptions.sessionManagementTableName)
-      } else if (tokenMappingItem !== null && tokenMappingItem?.sessionId === sessionId) {
+      } else {
         authResult = await authenticateRequest(username, axiosInstance, ddbClient, logger,
           authOptions, authOptions.tokenMappingTableName)
       }
