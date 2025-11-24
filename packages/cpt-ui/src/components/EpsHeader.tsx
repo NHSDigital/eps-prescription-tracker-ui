16e775c068e9411dac707c5e7ef4c0f0bc4380df
Jonno <jonathan.welch1@nhs.net>
2025-11-24 11:43:25 +0000

    refactored return statement to make logic of header work and links

--- packages/cpt-ui/src/components/EpsHeader.tsx
+++ packages/cpt-ui/src/components/EpsHeader.tsx
@@ -1,213 +1,213 @@
-// import React, {
-//   Fragment,
-//   useContext,
-//   useEffect,
-//   useState
-// } from "react"
-// import {Link, useNavigate, useLocation} from "react-router-dom"
-// import {Header} from "nhsuk-react-components"
-// import {
-//   HEADER_SERVICE,
-//   HEADER_EXIT_BUTTON,
-//   HEADER_EXIT_TARGET,
-//   HEADER_CHANGE_ROLE_BUTTON,
-//   HEADER_CHANGE_ROLE_TARGET,
-//   HEADER_SELECT_YOUR_ROLE_TARGET,
-//   HEADER_SELECT_YOUR_ROLE_BUTTON,
-//   HEADER_FEEDBACK_BUTTON,
-//   HEADER_FEEDBACK_TARGET,
-//   HEADER_LOG_OUT_BUTTON
-// } from "@/constants/ui-strings/HeaderStrings"
+import React, {
+  Fragment,
+  useContext,
+  useEffect,
+  useState
+} from "react"
+import {Link, useNavigate, useLocation} from "react-router-dom"
+import {Header} from "nhsuk-react-components"
+import {
+  HEADER_SERVICE,
+  HEADER_EXIT_BUTTON,
+  HEADER_EXIT_TARGET,
+  HEADER_CHANGE_ROLE_BUTTON,
+  HEADER_CHANGE_ROLE_TARGET,
+  HEADER_SELECT_YOUR_ROLE_TARGET,
+  HEADER_SELECT_YOUR_ROLE_BUTTON,
+  HEADER_FEEDBACK_BUTTON,
+  HEADER_FEEDBACK_TARGET,
+  HEADER_LOG_OUT_BUTTON
+} from "@/constants/ui-strings/HeaderStrings"
 
-// import {AuthContext} from "@/context/AuthProvider"
-// import {useAuth} from "@/context/AuthProvider"
+import {AuthContext} from "@/context/AuthProvider"
+import {useAuth} from "@/context/AuthProvider"
 
-// import {EpsLogoutModal} from "@/components/EpsLogoutModal"
-// import {normalizePath} from "@/helpers/utils"
-// import {ENV_CONFIG, FRONTEND_PATHS, AUTH_CONFIG} from "@/constants/environment"
-// import {getHomeLink} from "@/helpers/loginFunctions"
-// import {signOut} from "@/helpers/logout"
+import {EpsLogoutModal} from "@/components/EpsLogoutModal"
+import {normalizePath} from "@/helpers/utils"
+import {ENV_CONFIG, FRONTEND_PATHS, AUTH_CONFIG} from "@/constants/environment"
+import {getHomeLink} from "@/helpers/loginFunctions"
+import {signOut} from "@/helpers/logout"
 
-// const baseUrl = ENV_CONFIG.BASE_URL
+const baseUrl = ENV_CONFIG.BASE_URL
 
-// export default function EpsHeader() {
-//   const navigate = useNavigate()
-//   const location = useLocation()
-//   // TODO: REFACTOR REQUIRED
-//   // We should only useAuth instead of useContext...
-//   const auth = useContext(AuthContext)
-//   const authContext = useAuth()
+export default function EpsHeader() {
+  const navigate = useNavigate()
+  const location = useLocation()
+  // TODO: REFACTOR REQUIRED
+  // We should only useAuth instead of useContext...
+  const auth = useContext(AuthContext)
+  const authContext = useAuth()
 
-//   // Individual states to control link visibility:
-//   const [shouldShowSelectRole, setShouldShowSelectRole] = useState(false)
-//   const [shouldShowChangeRole, setShouldShowChangeRole] = useState(false)
-//   const [shouldShowLogoutLink, setShouldShowLogoutLink] = useState(false)
-//   const [shouldShowExitButton, setShouldShowExitButton] = useState(false)
+  // Individual states to control link visibility:
+  const [shouldShowSelectRole, setShouldShowSelectRole] = useState(false)
+  const [shouldShowChangeRole, setShouldShowChangeRole] = useState(false)
+  const [shouldShowLogoutLink, setShouldShowLogoutLink] = useState(false)
+  const [shouldShowExitButton, setShouldShowExitButton] = useState(false)
 
-//   const [showLogoutModal, setShowLogoutModal] = useState(false)
+  const [showLogoutModal, setShowLogoutModal] = useState(false)
 
-//   // Move all conditional logic into one place
-//   useEffect(() => {
-//     const isSignedIn = auth?.isSignedIn as boolean
+  // Move all conditional logic into one place
+  useEffect(() => {
+    const isSignedIn = auth?.isSignedIn as boolean
 
-//     const path = normalizePath(location.pathname)
+    const path = normalizePath(location.pathname)
 
-//     // Show "Select your role" link
-//     setShouldShowSelectRole(
-//       path !== FRONTEND_PATHS.SESSION_SELECTION &&
-//       path !== FRONTEND_PATHS.SELECT_YOUR_ROLE &&
-//       path !== FRONTEND_PATHS.CHANGE_YOUR_ROLE &&
-//       path !== FRONTEND_PATHS.LOGOUT &&
-//       path !== FRONTEND_PATHS.SESSION_LOGGED_OUT &&
-//       isSignedIn &&
-//       !authContext.hasSingleRoleAccess &&
-//       !authContext.selectedRole
-//     )
+    // Show "Select your role" link
+    setShouldShowSelectRole(
+      path !== FRONTEND_PATHS.SESSION_SELECTION &&
+      path !== FRONTEND_PATHS.SELECT_YOUR_ROLE &&
+      path !== FRONTEND_PATHS.CHANGE_YOUR_ROLE &&
+      path !== FRONTEND_PATHS.LOGOUT &&
+      path !== FRONTEND_PATHS.SESSION_LOGGED_OUT &&
+      isSignedIn &&
+      !authContext.hasSingleRoleAccess &&
+      !authContext.selectedRole
+    )
 
-//     // Show "Change role" link (if not single access)
-//     setShouldShowChangeRole(
-//       path !== FRONTEND_PATHS.SESSION_SELECTION &&
-//       path !== FRONTEND_PATHS.SELECT_YOUR_ROLE &&
-//       path !== FRONTEND_PATHS.CHANGE_YOUR_ROLE &&
-//       path !== FRONTEND_PATHS.LOGOUT &&
-//       isSignedIn &&
-//       !authContext.hasSingleRoleAccess &&
-//       authContext.selectedRole !== undefined
-//     )
+    // Show "Change role" link (if not single access)
+    setShouldShowChangeRole(
+      path !== FRONTEND_PATHS.SESSION_SELECTION &&
+      path !== FRONTEND_PATHS.SELECT_YOUR_ROLE &&
+      path !== FRONTEND_PATHS.CHANGE_YOUR_ROLE &&
+      path !== FRONTEND_PATHS.LOGOUT &&
+      isSignedIn &&
+      !authContext.hasSingleRoleAccess &&
+      authContext.selectedRole !== undefined
+    )
 
-//     // Show the "Logout" link only if the user is signed in
-//     setShouldShowLogoutLink(Boolean(auth?.isSignedIn))
+    // Show the "Logout" link only if the user is signed in
+    setShouldShowLogoutLink(Boolean(auth?.isSignedIn))
 
-//     // Show the "Exit" button under these conditions
-//     setShouldShowExitButton(
-//       (path === FRONTEND_PATHS.LOGOUT && !auth?.isSignedIn) ||
-//       (path === FRONTEND_PATHS.SELECT_YOUR_ROLE && authContext.hasNoAccess) ||
-//       (path === "/notfound")
-//     )
-//   }, [location, auth, authContext])
+    // Show the "Exit" button under these conditions
+    setShouldShowExitButton(
+      (path === FRONTEND_PATHS.LOGOUT && !auth?.isSignedIn) ||
+      (path === FRONTEND_PATHS.SELECT_YOUR_ROLE && authContext.hasNoAccess) ||
+      (path === "/notfound")
+    )
+  }, [location, auth, authContext])
 
-//   const redirectToLogin = async (e: React.MouseEvent | React.KeyboardEvent) => {
-//     // Naked href don't respect the router, so this overrides that
-//     e.preventDefault()
-//     navigate(getHomeLink(auth?.isSignedIn || false))
-//   }
+  const redirectToLogin = async (e: React.MouseEvent | React.KeyboardEvent) => {
+    // Naked href don't respect the router, so this overrides that
+    e.preventDefault()
+    navigate(getHomeLink(auth?.isSignedIn || false))
+  }
 
-//   const handleLogoutClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
-//     e.preventDefault()
-//     setShowLogoutModal(true)
-//   }
+  const handleLogoutClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault()
+    setShowLogoutModal(true)
+  }
 
-//   const handleConfirmLogout = async () => {
-//     setShowLogoutModal(false)
-//     signOut(authContext, AUTH_CONFIG.REDIRECT_SIGN_OUT)
-//   }
+  const handleConfirmLogout = async () => {
+    setShowLogoutModal(false)
+    signOut(authContext, AUTH_CONFIG.REDIRECT_SIGN_OUT)
+  }
 
-//   return (
-//     <Fragment>
-//       <Header transactional className="masthead" id="eps-header">
-//         <Header.Container className="masthead-container">
-//           <Header.Logo href={`${baseUrl}${getHomeLink(auth?.isSignedIn || false)}`} />
-//           <Link
-//             to={getHomeLink(auth?.isSignedIn || false)}
-//             onClick={redirectToLogin}
-//             className="combined-logo-and-service-name"
-//             style={{
-//               display: "flex"
-//             }}
-//             data-testid="eps_header_logoLink"
-//           >
-//           </Link>
-//           <Header.ServiceName
-//             data-testid="eps_header_serviceName"
-//             href={`${baseUrl}${getHomeLink(auth?.isSignedIn || false)}`}>
-//             {HEADER_SERVICE}
-//           </Header.ServiceName>
-//           <Header.Content />
-//         </Header.Container>
+  return (
+    <Fragment>
+      <Header transactional className="masthead" id="eps-header">
+        <Header.Container className="masthead-container">
+          <Header.Logo href={`${baseUrl}${getHomeLink(auth?.isSignedIn || false)}`} />
+          <Link
+            to={getHomeLink(auth?.isSignedIn || false)}
+            onClick={redirectToLogin}
+            className="combined-logo-and-service-name"
+            style={{
+              display: "flex"
+            }}
+            data-testid="eps_header_logoLink"
+          >
+          </Link>
+          <Header.ServiceName
+            data-testid="eps_header_serviceName"
+            href={`${baseUrl}${getHomeLink(auth?.isSignedIn || false)}`}>
+            {HEADER_SERVICE}
+          </Header.ServiceName>
+          <Header.Content />
+        </Header.Container>
 
-//         <Header.Nav className="masthead-nav">
-//           {/* Select your role */}
-//           <div>
-//             {shouldShowSelectRole && (
-//               <li className="nhsuk-header__navigation-item">
-//                 <Link
-//                   className="nhsuk-header__navigation-link"
-//                   to={HEADER_SELECT_YOUR_ROLE_TARGET}
-//                   data-testid="eps_header_selectYourRoleLink"
-//                 >
-//                   {HEADER_SELECT_YOUR_ROLE_BUTTON}
-//                 </Link>
-//               </li>
-//             )}
-//           </div>
+        <Header.Nav className="masthead-nav">
+          {/* Select your role */}
+          <div>
+            {shouldShowSelectRole && (
+              <li className="nhsuk-header__navigation-item">
+                <Link
+                  className="nhsuk-header__navigation-link"
+                  to={HEADER_SELECT_YOUR_ROLE_TARGET}
+                  data-testid="eps_header_selectYourRoleLink"
+                >
+                  {HEADER_SELECT_YOUR_ROLE_BUTTON}
+                </Link>
+              </li>
+            )}
+          </div>
 
-//           {/* Change role */}
-//           <div>
-//             {shouldShowChangeRole && (
-//               <li className="nhsuk-header__navigation-item">
-//                 <Link
-//                   className="nhsuk-header__navigation-link"
-//                   to={HEADER_CHANGE_ROLE_TARGET}
-//                   data-testid="eps_header_changeRoleLink"
-//                 >
-//                   {HEADER_CHANGE_ROLE_BUTTON}
-//                 </Link>
-//               </li>
-//             )}
-//           </div>
+          {/* Change role */}
+          <div>
+            {shouldShowChangeRole && (
+              <li className="nhsuk-header__navigation-item">
+                <Link
+                  className="nhsuk-header__navigation-link"
+                  to={HEADER_CHANGE_ROLE_TARGET}
+                  data-testid="eps_header_changeRoleLink"
+                >
+                  {HEADER_CHANGE_ROLE_BUTTON}
+                </Link>
+              </li>
+            )}
+          </div>
 
-//           {/* Give feedback (opens in new tab) */}
-//           <div>
-//             <Header.NavItem
-//               href={HEADER_FEEDBACK_TARGET}
-//               target="_blank"
-//               rel="noopener noreferrer"
-//               data-testid="eps_header_feedbackLink"
-//             >
-//               {HEADER_FEEDBACK_BUTTON}
-//             </Header.NavItem>
-//           </div>
+          {/* Give feedback (opens in new tab) */}
+          <div>
+            <Header.NavItem
+              href={HEADER_FEEDBACK_TARGET}
+              target="_blank"
+              rel="noopener noreferrer"
+              data-testid="eps_header_feedbackLink"
+            >
+              {HEADER_FEEDBACK_BUTTON}
+            </Header.NavItem>
+          </div>
 
-//           {/* Log out */}
-//           <div>
-//             {shouldShowLogoutLink && (
-//               <li className="nhsuk-header__navigation-item">
-//                 <Link
-//                   className="nhsuk-header__navigation-link"
-//                   to={FRONTEND_PATHS.LOGOUT}
-//                   data-testid="eps_header_logout"
-//                   onClick={handleLogoutClick}
-//                 >
-//                   {HEADER_LOG_OUT_BUTTON}
-//                 </Link>
-//               </li>
-//             )}
-//           </div>
+          {/* Log out */}
+          <div>
+            {shouldShowLogoutLink && (
+              <li className="nhsuk-header__navigation-item">
+                <Link
+                  className="nhsuk-header__navigation-link"
+                  to={FRONTEND_PATHS.LOGOUT}
+                  data-testid="eps_header_logout"
+                  onClick={handleLogoutClick}
+                >
+                  {HEADER_LOG_OUT_BUTTON}
+                </Link>
+              </li>
+            )}
+          </div>
 
-//           {/* Exit button */}
-//           <div>
-//             {shouldShowExitButton && (
-//               <li className="nhsuk-header__navigation-item">
-//                 <Link
-//                   className="nhsuk-header__navigation-link"
-//                   to={HEADER_EXIT_TARGET}
-//                   data-testid="eps_header_exit"
-//                 >
-//                   {HEADER_EXIT_BUTTON}
-//                 </Link>
-//               </li>
-//             )}
-//           </div>
+          {/* Exit button */}
+          <div>
+            {shouldShowExitButton && (
+              <li className="nhsuk-header__navigation-item">
+                <Link
+                  className="nhsuk-header__navigation-link"
+                  to={HEADER_EXIT_TARGET}
+                  data-testid="eps_header_exit"
+                >
+                  {HEADER_EXIT_BUTTON}
+                </Link>
+              </li>
+            )}
+          </div>
 
-//           <Header.NavDropdownMenu dropdownText="Menu" />
-//         </Header.Nav>
-//       </Header>
+          <Header.NavDropdownMenu dropdownText="Menu" />
+        </Header.Nav>
+      </Header>
 
-//       <EpsLogoutModal
-//         isOpen={showLogoutModal}
-//         onClose={() => setShowLogoutModal(false)}
-//         onConfirm={handleConfirmLogout}
-//       />
-//     </Fragment>
-//   )
-// }
+      <EpsLogoutModal
+        isOpen={showLogoutModal}
+        onClose={() => setShowLogoutModal(false)}
+        onConfirm={handleConfirmLogout}
+      />
+    </Fragment>
+  )
+}
