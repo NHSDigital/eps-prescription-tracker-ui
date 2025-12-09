3591ea310cd7586c4004c2c66ac591e94b6565c9
Jonno <jonathan.welch1@nhs.net>
2025-12-09 09:45:21 +0000

    readded package lock and reverted change in layout improt

--- packages/cpt-ui/src/Layout.tsx
+++ packages/cpt-ui/src/Layout.tsx
@@ -1,10 +1,10 @@
 import {Outlet} from "react-router-dom"
+import EpsHeader from "@/components/EpsHeader"
 import RBACBanner from "@/components/RBACBanner"
 import EpsFooter from "@/components/EpsFooter"
 import PatientDetailsBanner from "@/components/PatientDetailsBanner"
 import PrescriptionInformationBanner from "@/components/PrescriptionInformationBanner"
 import {Fragment} from "react"
-import EpsHeader from "./components/EpsHeader"
 
 export default function Layout() {
   return (
