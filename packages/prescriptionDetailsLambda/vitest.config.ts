77f6a825ece83099495c13061bc25018aad65ced
Anthony Brown <anthony.brown8@nhs.net>
2026-01-12 09:05:01 +0000

    more tidy

--- packages/prescriptionDetailsLambda/vitest.config.ts
+++ packages/prescriptionDetailsLambda/vitest.config.ts
@@ -1,15 +1,36 @@
 import {defineConfig, mergeConfig} from "vitest/config"
 import sharedVitestConfig from "../../vitest.base.config"
 
+const apigeeHost = "https://dummyApigee"
+const CIS2_OIDC_HOST = "https://dummy_cis2_auth.com"
+const MOCK_OIDC_HOST = "https://dummy_mock_auth.com"
 export default mergeConfig(sharedVitestConfig, defineConfig({
   test: {
     dir: "./tests",
-    setupFiles: ["./.jest/setEnvVars.js"],
-    threads: false
-  },
-  server: {
-    deps: {
-      inline: ["nock"]
+    env: {
+      apigeeHost: apigeeHost,
+      apigeeCIS2TokenEndpoint: `${apigeeHost}/cis2_token`,
+      apigeeMockTokenEndpoint: `${apigeeHost}/mock_token`,
+      apigeePrescriptionsEndpoint: `${apigeeHost}/prescriptions`,
+      TokenMappingTableName: "dummyTable",
+      jwtPrivateKeyArn: "dummy_jwtPrivateKeyArn",
+      jwtKid: "jwt_kid",
+      roleId: "dummy_role",
+      MOCK_MODE_ENABLED: "true",
+      CIS2_OIDC_ISSUER: "valid_cis2_iss",
+      CIS2_OIDC_CLIENT_ID: "valid_cis2_aud",
+      CIS2_OIDC_HOST: CIS2_OIDC_HOST,
+      CIS2_OIDCJWKS_ENDPOINT: `${CIS2_OIDC_HOST}/.well-known/jwks.json`,
+      CIS2_USER_INFO_ENDPOINT: `${CIS2_OIDC_HOST}/userinfo`,
+      CIS2_USER_POOL_IDP: "CIS2DummyPoolIdentityProvider",
+      CIS2_IDP_TOKEN_PATH: `${CIS2_OIDC_HOST}/token`,
+      MOCK_OIDC_ISSUER: "valid_mock_iss",
+      MOCK_OIDC_CLIENT_ID: "valid_mock_aud",
+      MOCK_OIDC_HOST: MOCK_OIDC_HOST,
+      MOCK_OIDCJWKS_ENDPOINT: `${MOCK_OIDC_HOST}/.well-known/jwks.json`,
+      MOCK_USER_INFO_ENDPOINT: `${MOCK_OIDC_HOST}/userinfo`,
+      MOCK_USER_POOL_IDP: "MockDummyPoolIdentityProvider",
+      MOCK_IDP_TOKEN_PATH: `${MOCK_OIDC_HOST}/token`
     }
   }
 }))
