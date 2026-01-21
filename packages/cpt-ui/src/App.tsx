f1961c4dbbafae6633acaa962a1955fdeee8257d
Jonno <jonathan.welch1@nhs.net>
2026-01-21 17:02:16 +0000

    fixed it so skip link is first tabbable item

--- packages/cpt-ui/src/App.tsx
+++ packages/cpt-ui/src/App.tsx
@@ -6,6 +6,7 @@ import {NavigationProvider} from "@/context/NavigationProvider"
 import {PatientDetailsProvider} from "./context/PatientDetailsProvider"
 import {PrescriptionInformationProvider} from "./context/PrescriptionInformationProvider"
 import Layout from "@/Layout"
+import {useEffect} from "react"
 
 import LoginPage from "@/pages/LoginPage"
 import LogoutPage from "@/pages/LogoutPage"
@@ -33,6 +34,34 @@ import {HEADER_STRINGS} from "@/constants/ui-strings/HeaderStrings"
 function AppContent() {
   const location = useLocation()
 
+  // this useEffect ensures that focus starts with skip link when using tab navigation
+  useEffect(() => {
+    let hasTabbed = false
+
+    const activeElement = document.activeElement as HTMLElement
+    if (activeElement && activeElement !== document.body) {
+      activeElement.blur()
+    }
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Tab" && !hasTabbed && !e.shiftKey) {
+        hasTabbed = true
+        e.preventDefault()
+        const skipLink = document.querySelector(".nhsuk-skip-link") as HTMLElement
+        if (skipLink) {
+          skipLink.focus()
+        }
+        document.removeEventListener("keydown", handleKeyDown)
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown)
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [location.pathname])
+
   // Check if we're on a prescription list or prescription details page
   const isPrescriptionPage =
     location.pathname === FRONTEND_PATHS.PRESCRIPTION_LIST_CURRENT ||
@@ -43,8 +72,7 @@ function AppContent() {
   const skipTarget = isPrescriptionPage ? "#patient-details-banner" : "#main-content"
 
   return (
-    <PatientDetailsProvider>
-      <EPSCookieBanner />
+    <>
       <a
         href={skipTarget}
         className="nhsuk-skip-link"
@@ -52,42 +80,45 @@ function AppContent() {
       >
         {HEADER_STRINGS.SKIP_TO_MAIN_CONTENT}
       </a>
-      <PrescriptionInformationProvider>
-        <SearchProvider>
-          <NavigationProvider>
-            <Routes>
-              <Route path="/" element={<Layout />}>
-                {/* Public cookie routes */}
-                <Route path="cookies" element={<CookiePolicyPage />} />
-                <Route path="cookies-selected" element={<CookieSettingsPage />} />
+      <PatientDetailsProvider>
+        <EPSCookieBanner />
+        <PrescriptionInformationProvider>
+          <SearchProvider>
+            <NavigationProvider>
+              <Routes>
+                <Route path="/" element={<Layout />}>
+                  {/* Public cookie routes */}
+                  <Route path="cookies" element={<CookiePolicyPage />} />
+                  <Route path="cookies-selected" element={<CookieSettingsPage />} />
 
-                {/* Your existing routes */}
-                <Route path="*" element={<NotFoundPage />} />
-                <Route path={FRONTEND_PATHS.SESSION_SELECTION} element={<SessionSelectionPage />} />
-                <Route path={FRONTEND_PATHS.LOGIN} element={<LoginPage />} />
-                <Route path={FRONTEND_PATHS.LOGOUT} element={<LogoutPage />} />
-                <Route path={FRONTEND_PATHS.SESSION_LOGGED_OUT} element={<SessionLoggedOutPage />} />
-                <Route path={FRONTEND_PATHS.SELECT_YOUR_ROLE} element={<SelectYourRolePage />} />
-                <Route path={FRONTEND_PATHS.YOUR_SELECTED_ROLE} element={<YourSelectedRolePage />} />
-                <Route path={FRONTEND_PATHS.CHANGE_YOUR_ROLE} element={<ChangeRolePage />} />
-                <Route path={FRONTEND_PATHS.SEARCH_BY_PRESCRIPTION_ID} element={<SearchPrescriptionPage />} />
-                <Route path={FRONTEND_PATHS.SEARCH_BY_NHS_NUMBER} element={<SearchPrescriptionPage />} />
-                <Route path={FRONTEND_PATHS.SEARCH_BY_BASIC_DETAILS} element={<SearchPrescriptionPage />} />
-                <Route path={FRONTEND_PATHS.PRESCRIPTION_LIST_CURRENT} element={<PrescriptionListPage />} />
-                <Route path={FRONTEND_PATHS.PRESCRIPTION_LIST_FUTURE} element={<PrescriptionListPage />} />
-                <Route path={FRONTEND_PATHS.PRESCRIPTION_LIST_PAST} element={<PrescriptionListPage />} />
-                <Route path={FRONTEND_PATHS.PRESCRIPTION_DETAILS_PAGE} element={<PrescriptionDetailsPage />} />
-                <Route path={FRONTEND_PATHS.PATIENT_SEARCH_RESULTS} element={<SearchResultsPage />} />
-                <Route path={FRONTEND_PATHS.TOO_MANY_SEARCH_RESULTS} element={<TooManySearchResultsPage />} />
-                <Route path={FRONTEND_PATHS.NO_PATIENT_FOUND} element={<NoPatientsFoundPage />} />
-                <Route path={FRONTEND_PATHS.NO_PRESCRIPTIONS_FOUND} element={<NoPrescriptionsFoundPage />} />
-                <Route path={FRONTEND_PATHS.PRIVACY_NOTICE} element={<PrivacyNoticePage />} />
-              </Route>
-            </Routes>
-          </NavigationProvider>
-        </SearchProvider>
-      </PrescriptionInformationProvider>
-    </PatientDetailsProvider>
+                  {/* Your existing routes */}
+                  <Route path="*" element={<NotFoundPage />} />
+                  <Route path={FRONTEND_PATHS.SESSION_SELECTION} element={<SessionSelectionPage />} />
+                  <Route path={FRONTEND_PATHS.LOGIN} element={<LoginPage />} />
+                  <Route path={FRONTEND_PATHS.LOGOUT} element={<LogoutPage />} />
+                  <Route path={FRONTEND_PATHS.SESSION_LOGGED_OUT} element={<SessionLoggedOutPage />} />
+                  <Route path={FRONTEND_PATHS.SELECT_YOUR_ROLE} element={<SelectYourRolePage />} />
+                  <Route path={FRONTEND_PATHS.YOUR_SELECTED_ROLE} element={<YourSelectedRolePage />} />
+                  <Route path={FRONTEND_PATHS.CHANGE_YOUR_ROLE} element={<ChangeRolePage />} />
+                  <Route path={FRONTEND_PATHS.SEARCH_BY_PRESCRIPTION_ID} element={<SearchPrescriptionPage />} />
+                  <Route path={FRONTEND_PATHS.SEARCH_BY_NHS_NUMBER} element={<SearchPrescriptionPage />} />
+                  <Route path={FRONTEND_PATHS.SEARCH_BY_BASIC_DETAILS} element={<SearchPrescriptionPage />} />
+                  <Route path={FRONTEND_PATHS.PRESCRIPTION_LIST_CURRENT} element={<PrescriptionListPage />} />
+                  <Route path={FRONTEND_PATHS.PRESCRIPTION_LIST_FUTURE} element={<PrescriptionListPage />} />
+                  <Route path={FRONTEND_PATHS.PRESCRIPTION_LIST_PAST} element={<PrescriptionListPage />} />
+                  <Route path={FRONTEND_PATHS.PRESCRIPTION_DETAILS_PAGE} element={<PrescriptionDetailsPage />} />
+                  <Route path={FRONTEND_PATHS.PATIENT_SEARCH_RESULTS} element={<SearchResultsPage />} />
+                  <Route path={FRONTEND_PATHS.TOO_MANY_SEARCH_RESULTS} element={<TooManySearchResultsPage />} />
+                  <Route path={FRONTEND_PATHS.NO_PATIENT_FOUND} element={<NoPatientsFoundPage />} />
+                  <Route path={FRONTEND_PATHS.NO_PRESCRIPTIONS_FOUND} element={<NoPrescriptionsFoundPage />} />
+                  <Route path={FRONTEND_PATHS.PRIVACY_NOTICE} element={<PrivacyNoticePage />} />
+                </Route>
+              </Routes>
+            </NavigationProvider>
+          </SearchProvider>
+        </PrescriptionInformationProvider>
+      </PatientDetailsProvider>
+    </>
   )
 }
 
