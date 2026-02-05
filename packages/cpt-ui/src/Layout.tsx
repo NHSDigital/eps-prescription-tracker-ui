f74b8d38ec4c47b6c55cc4e2c77e44fe88364312
Connor Avery <connor.avery2@nhs.net>
2026-02-05 13:51:14 +0000

    New: [AEA-6209] - Render a loading page if rendering is blocked (#1750)
    ## Summary
    
    - :sparkles: New Feature
    
    ### Details
    
    Render a loading page to illustrate to the user that something is
    happening, in the event that a render is currently blocked while state
    is being updated due to:
    
    1. Login
    2. Logout
    3. Session time-outs
    4. State malfunction (unknown cause)
    
    ---------
    
    Signed-off-by: Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>

--- packages/cpt-ui/src/Layout.tsx
+++ packages/cpt-ui/src/Layout.tsx
@@ -4,15 +4,15 @@ import RBACBanner from "@/components/RBACBanner"
 import EpsFooter from "@/components/EpsFooter"
 import PatientDetailsBanner from "@/components/PatientDetailsBanner"
 import PrescriptionInformationBanner from "@/components/PrescriptionInformationBanner"
-import {Fragment} from "react"
+import {Fragment, ReactNode} from "react"
 
-export default function Layout() {
+export default function Layout({children}: {children?: ReactNode}) {
   return (
     <Fragment>
       <EpsHeader />
-      <PatientDetailsBanner />
-      <PrescriptionInformationBanner />
-      <Outlet />
+      {!children && <PatientDetailsBanner />}
+      {!children && <PrescriptionInformationBanner />}
+      {children ?? <Outlet />}
       <RBACBanner />
       <EpsFooter />
     </Fragment>
