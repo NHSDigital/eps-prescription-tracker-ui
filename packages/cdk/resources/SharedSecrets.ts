4947f7b24c46cad2ef542cdc9db5060c54939d54
anthony-nhs <121869075+anthony-nhs@users.noreply.github.com>
2026-02-06 11:28:17 +0000

    Chore: [AEA-6051] - add secrets for api keys (#1749)
    ## Summary
    
    - Routine Change
    
    ### Details
    
    - store api key secrets in secrets rather than lambda environment
    variables

--- packages/cdk/resources/SharedSecrets.ts
+++ packages/cdk/resources/SharedSecrets.ts
@@ -16,6 +16,9 @@ export interface SharedSecretsProps {
   readonly stackName: string
   readonly deploymentRole: IRole
   readonly useMockOidc?: boolean
+  readonly apigeeApiKey: string
+  readonly apigeeApiSecret: string
+  readonly apigeeDoHSApiKey: string
 }
 
 // Construct for managing shared secrets and associated resources
@@ -26,6 +29,11 @@ export class SharedSecrets extends Construct {
   public readonly useJwtKmsKeyPolicy: ManagedPolicy
   public readonly getPrimaryJwtPrivateKeyPolicy: ManagedPolicy
   public readonly getMockJwtPrivateKeyPolicy: ManagedPolicy
+  public readonly apigeeSecretsKmsKey: IKey
+  public readonly apigeeApiKey: Secret
+  public readonly apigeeApiSecret: Secret
+  public readonly apigeeDoHSApiKey: Secret
+  public readonly getApigeeSecretsPolicy: ManagedPolicy
 
   constructor(scope: Construct, id: string, props: SharedSecretsProps) {
     super(scope, id)
@@ -55,6 +63,65 @@ export class SharedSecrets extends Construct {
           })
         ]
       })
+    })
+
+    this.apigeeSecretsKmsKey = new Key(this, "ApigeeSecretsKmsKey", {
+      description: `${props.stackName}-apigeeSecretsKmsKey`,
+      enableKeyRotation: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      pendingWindow: Duration.days(7),
+      policy: new PolicyDocument({
+        statements: [
+          // Allow full IAM permissions for account root
+          new PolicyStatement({
+            sid: "EnableIAMUserPermissions",
+            effect: Effect.ALLOW,
+            actions: ["kms:*"],
+            principals: [new AccountRootPrincipal()],
+            resources: ["*"]
+          }),
+          // Allow the deployment role to encrypt and generate data keys
+          new PolicyStatement({
+            effect: Effect.ALLOW,
+            principals: [props.deploymentRole],
+            actions: ["kms:Encrypt", "kms:GenerateDataKey*"],
+            resources: ["*"]
+          })
+        ]
+      })
+    })
+    this.apigeeApiKey = new Secret(this, "ApigeeApiKey", {
+      secretName: `${props.stackName}-apigeeApiKey`,
+      secretStringValue: SecretValue.unsafePlainText(props.apigeeApiKey),
+      encryptionKey: this.apigeeSecretsKmsKey
+    })
+    this.apigeeApiSecret = new Secret(this, "ApigeeApiSecret", {
+      secretName: `${props.stackName}-apigeeApiSecret`,
+      secretStringValue: SecretValue.unsafePlainText(props.apigeeApiSecret),
+      encryptionKey: this.apigeeSecretsKmsKey
+    })
+    this.apigeeDoHSApiKey = new Secret(this, "ApigeeDoHSApiKey", {
+      secretName: `${props.stackName}-apigeeDoHSApiKey`,
+      secretStringValue: SecretValue.unsafePlainText(props.apigeeDoHSApiKey),
+      encryptionKey: this.apigeeSecretsKmsKey
+    })
+
+    // Create a managed policy to allow getting the primary JWT private key secret
+    this.getApigeeSecretsPolicy = new ManagedPolicy(this, "GetApigeeSecretsPolicy", {
+      statements: [
+        new PolicyStatement({
+          actions: ["secretsmanager:GetSecretValue"],
+          resources: [
+            this.apigeeApiKey.secretArn,
+            this.apigeeApiSecret.secretArn,
+            this.apigeeDoHSApiKey.secretArn]
+        }),
+        new PolicyStatement({
+          actions: ["kms:DescribeKey", "kms:Decrypt"],
+          effect: Effect.ALLOW,
+          resources: [this.apigeeSecretsKmsKey.keyArn]
+        })
+      ]
     })
 
     // Create a managed policy to allow using the KMS key for decryption
