1e8b6041f56574547d5f7031e52af7781157c073
Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>
2026-02-26 10:58:45 +0000

    missed one
    Signed-off-by: Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>

--- packages/cpt-ui/__tests__/mocks/AuthStateMock.tsx
+++ packages/cpt-ui/__tests__/mocks/AuthStateMock.tsx
@@ -23,7 +23,7 @@ export const mockAuthState = {
   isConcurrentSession: false,
   invalidSessionCause: undefined,
   sessionId: "test-session-id",
-  deviceId: "test-desktop-id",
+  deviceId: "test-device-id",
   rolesWithAccess: [],
   rolesWithoutAccess: [],
   selectedRole: undefined,
