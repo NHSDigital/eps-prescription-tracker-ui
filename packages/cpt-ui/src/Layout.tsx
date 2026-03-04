afc1fb30426e63054f2027d7f5c769a9a44c23c0
Jonno <jonathan.welch1@nhs.net>
2026-03-04 15:55:12 +0000

    not showing on incorrect pages

--- packages/cpt-ui/src/Layout.tsx
+++ packages/cpt-ui/src/Layout.tsx
@@ -1,17 +1,32 @@
-import {Outlet} from "react-router-dom"
+import {Outlet, useLocation} from "react-router-dom"
 import EpsHeader from "@/components/EpsHeader"
 import RBACBanner from "@/components/RBACBanner"
 import EpsFooter from "@/components/EpsFooter"
 import PatientDetailsBanner from "@/components/PatientDetailsBanner"
 import PrescriptionInformationBanner from "@/components/PrescriptionInformationBanner"
 import {Fragment, ReactNode} from "react"
+import {FRONTEND_PATHS} from "@/constants/environment"
+import {normalizePath} from "@/helpers/utils"
 
 export default function Layout({children}: {children?: ReactNode}) {
+  const location = useLocation()
+
+  // Define pages where patient and prescription banners should be shown
+  const bannerAllowedPaths = [
+    FRONTEND_PATHS.PRESCRIPTION_LIST_CURRENT,
+    FRONTEND_PATHS.PRESCRIPTION_LIST_PAST,
+    FRONTEND_PATHS.PRESCRIPTION_LIST_FUTURE,
+    FRONTEND_PATHS.PRESCRIPTION_DETAILS_PAGE
+  ]
+
+  const currentPath = normalizePath(location.pathname)
+  const shouldShowBanners = !children && bannerAllowedPaths.includes(currentPath)
+
   return (
     <Fragment>
       <EpsHeader />
-      {!children && <PatientDetailsBanner />}
-      {!children && <PrescriptionInformationBanner />}
+      {shouldShowBanners && <PatientDetailsBanner />}
+      {shouldShowBanners && <PrescriptionInformationBanner />}
       {children ?? <Outlet />}
       <RBACBanner />
       <EpsFooter />
