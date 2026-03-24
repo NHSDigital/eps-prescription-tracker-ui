d2dd3ca1a3d518abd78672dcb183cb5b56729e79
Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>
2026-03-24 14:06:47 +0000

    Strings WIP
    Signed-off-by: Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>

--- packages/cpt-ui/src/pages/AccessibilityStatementPage.tsx
+++ packages/cpt-ui/src/pages/AccessibilityStatementPage.tsx
@@ -4,7 +4,7 @@ import {getHomeLink} from "@/helpers/loginFunctions"
 import {Breadcrumb} from "nhsuk-react-components"
 import {useAuth} from "@/context/AuthProvider"
 import {usePageTitle} from "@/hooks/usePageTitle"
-import {AccessibilityStatementStrings} from "@/constants/ui-strings/AccessibilityStatement"
+import {AccessibilityStatementStrings} from "@/constants/ui-strings/AccessibilityStatementStrings"
 
 export default function AccessibilityStatementPage() {
   const auth = useAuth()
@@ -27,11 +27,43 @@ export default function AccessibilityStatementPage() {
           <h1 className="nhsuk-heading-xl">{AccessibilityStatementStrings.header}</h1>
 
           <h2 className="nhsuk-heading-l">{AccessibilityStatementStrings.intro.header}</h2>
-          <p>{AccessibilityStatementStrings.intro.welcome}</p>
+          <p>This accessibility statement applies to the Prescription Tracker.
+This website is run by NHS England. We want as many people as possible to be able to use this website.
+For example, that means you should be able to:<br /></p>
+          <ul>
+            <li>change colours, contrast levels and fonts using browser or device settings</li>
+            <li>zoom in up to 400% without the text spilling off the screen</li>
+            <li>navigate most of the website using a keyboard or speech recognition software</li>
+            <li>
+              listen to most of the website using a screen reader (including the most recent versions of JAWS, NVDA
+              and VoiceOver)
+            </li>
+          </ul>
+
+          <p>
+            We’ve also made the website text as simple as possible to understand.
+
+            <a href="" target="_blank">AbilityNet (opens in new tab) </a>
+            has advice on making your device easier to use if you have a disability.
+          </p>
 
           <h2 className="nhsuk-heading-l">
-            {AccessibilityStatementStrings.contact.header}
+            How accessible this website is
           </h2>
+          <p>
+            We know some parts of this website are not fully accessible:
+          </p>
+          <ul>
+            <li>screenreaders do not continue to read content when you select a new tab on the ‘Search for a prescription’ page</li>
+            <li>using the tab key does not focus the ‘Skip to main content’ link after a page is loaded</li>
+            <li>if you click into a page after it is loaded, using the tab key does not focus the next element of the page</li>
+            <li>the hint text is not associated with the search field on the prescription ID tab on the ‘Search for a prescription’ page</li>
+            <li>the cards on the ‘Select your role’ page should stay white when focused or hovered over</li>
+            <li>on smaller screens, there is no aria-expanded attribute on the menu icon</li>
+            <li>when you enter information into a field on the ‘Search for a prescription’ page and search, the page refreshes and focus is moved to the footer if you are navigating using arrow keys</li>
+            <li>the tabs on the ‘Prescription List’ page are separate URLs with the same headings and page titles, instead of the same URL with different fragment identifiers</li>
+            <li>selecting the tabs on the ‘Search for a prescription’ page should show you unique headings and page titles because each tab is a separate URL</li>
+          </ul>
         </div>
       </div>
     </main>
