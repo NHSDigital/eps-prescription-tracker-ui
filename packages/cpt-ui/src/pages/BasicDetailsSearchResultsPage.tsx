c5b1564052fb03d83d9214ae5ec09299d5299a3e
Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>
2026-03-30 11:14:47 +0000

    Update comment
    Signed-off-by: Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>

--- packages/cpt-ui/src/pages/BasicDetailsSearchResultsPage.tsx
+++ packages/cpt-ui/src/pages/BasicDetailsSearchResultsPage.tsx
@@ -152,8 +152,8 @@ export default function SearchResultsPage() {
       if (payload.length === 1) {
         const relevantParams =
           navigationContext.getRelevantSearchParameters("basicDetails")
-        // Set the search type before attempting search again
-        // As we'll now be using the nhs number search
+        // Set the search type before passing to prescription list current page
+        // So that the useEffect will detect and treat the search type correctly
         searchContext.setAllSearchParameters({
           ...relevantParams,
           nhsNumber: payload[0].nhsNumber,
