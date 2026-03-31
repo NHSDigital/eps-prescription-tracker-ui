e0682c7a6ee65206c35a0510409c70cac9138eb0
Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>
2026-03-31 09:11:34 +0000

    Basic search test adjustments
    Signed-off-by: Connor Avery <214469360+connoravo-nhs@users.noreply.github.com>

--- packages/cpt-ui/__tests__/BasicDetailsSearchResultsPage.test.tsx
+++ packages/cpt-ui/__tests__/BasicDetailsSearchResultsPage.test.tsx
@@ -431,7 +431,8 @@ describe("BasicDetailsSearchResultsPage", () => {
         dobMonth: "01",
         dobYear: "1990",
         postcode: "SW1A 1AA",
-        nhsNumber: "9726919207"
+        nhsNumber: "9726919207",
+        searchType: "basicDetails"
       })
     })
   })
@@ -457,7 +458,8 @@ describe("BasicDetailsSearchResultsPage", () => {
         dobMonth: "01",
         dobYear: "1990",
         postcode: "SW1A 1AA",
-        nhsNumber: "9726919207"
+        nhsNumber: "9726919207",
+        searchType: "basicDetails"
       })
     })
   })
@@ -499,7 +501,8 @@ describe("BasicDetailsSearchResultsPage", () => {
         dobMonth: "01",
         dobYear: "1990",
         postcode: "SW1A 1AA",
-        nhsNumber: "9726919207"
+        nhsNumber: "9726919207",
+        searchType: "basicDetails"
       })
     })
   })
@@ -525,7 +528,8 @@ describe("BasicDetailsSearchResultsPage", () => {
         dobMonth: "01",
         dobYear: "1990",
         postcode: "SW1A 1AA",
-        nhsNumber: "9726919207"
+        nhsNumber: "9726919207",
+        searchType: "basicDetails"
       })
     })
   })
