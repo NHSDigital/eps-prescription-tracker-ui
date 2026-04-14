24ae19760861cd9379d574bde3c0d42bb0ea25d4
Adam Brown <adam.brown41@nhs.net>
2026-04-14 15:59:02 +0000

    [AEA-65504] updates UI to support the old and new format of the pending cancellation extension on both responses

--- packages/common/commonTypes/src/prescriptionList.ts
+++ packages/common/commonTypes/src/prescriptionList.ts
@@ -38,8 +38,9 @@ export interface PrescriptionSummary {
   prescriptionTreatmentType: TreatmentType
   issueNumber?: number
   maxRepeats?: number
-  prescriptionPendingCancellation: boolean
-  itemsPendingCancellation: boolean
+  // prescriptionPendingCancellation: boolean
+  // itemsPendingCancellation: boolean
+  pendingCancellation: boolean
   nhsNumber?: string
 }
 
